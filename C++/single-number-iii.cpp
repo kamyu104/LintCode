1491b8a8018fe14ab36b3e00d1a2cb9af1231c32
Allen Liu <kamyu104@gmail.com>
2015-04-19 15:03:45 +0800

    update

---
+++ C++/single-number-iii.cpp
@@ -0,0 +1,32 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    /**
+     * @param A : An integer array
+     * @return : Two integers
+     */
+    vector<int> singleNumberIII(vector<int> &A) {
+        // Xor all the elements to get x ^ y.
+        int x_xor_y = 0;
+        for (auto& i : A) {
+            x_xor_y ^= i;
+        }
+        
+        // Get the last bit where 1 occurs.
+        auto bit = x_xor_y & ~(x_xor_y - 1);
+        
+        // Get the subset of A where the number has the bit.
+        // The subset only contains one of the two integers, call it x.
+        // Xor all the elemets in the subset to get x.
+        int x = 0;
+        for (auto& i : A) {
+            if (i & bit) {
+                x ^= i;
+            }
+        }
+        
+        return vector<int>{x, x_xor_y ^ x};
+    }
+};
