1e8a8854ea45668a61465239fc0ad845083aa142
Allen Liu <kamyu104@gmail.com>
2015-04-21 00:38:43 +0800

    add solution

---
+++ C++/majority-number.cpp
@@ -0,0 +1,27 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    /**
+     * @param nums: A list of integers
+     * @return: The majority number
+     */
+    int majorityNumber(vector<int> nums) {
+        int ans = nums[0], cnt = 1;
+        for (auto& i : nums) {
+            if (i == ans) {
+                ++cnt;
+            }
+            else {
+                --cnt;
+                if(cnt == 0) {
+                    ans = i;
+                    cnt = 2;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
