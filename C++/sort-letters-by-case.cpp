4eb6e3c382daaea141640cb7c4a608560f4ba2e6
kamyu <kamyu104@users.noreply.github.com>
2015-04-22 15:27:57 +0800

    Create sort-letters-by-case.cpp

---
+++ C++/sort-letters-by-case.cpp
@@ -0,0 +1,23 @@
+// Time:   O(n)
+// Spacee: O(1)
+
+class Solution {
+public:
+    /** 
+     * @param chars: The letters array you should sort.
+     */
+    void sortLetters(string &letters) {
+        int left = 0, right = letters.size() - 1;
+        
+        while (left < right) {
+            if (isupper(letters[left])) {
+                swap(letters[left], letters[right]);
+                --right;
+            }
+            else {
+                ++left;
+            }
+        }
+    }
+};
+
