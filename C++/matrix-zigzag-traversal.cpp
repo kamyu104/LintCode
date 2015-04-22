b5ea0571a4e2803839bbd7e5d9fa580967a8ddb4
kamyu <kamyu104@users.noreply.github.com>
2015-04-22 12:44:41 +0800

    Update matrix-zigzag-traversal.cpp

--- C++/matrix-zigzag-traversal.cpp
+++ C++/matrix-zigzag-traversal.cpp
@@ -9,6 +9,36 @@ public:
      */
     vector<int> printZMatrix(vector<vector<int> > &matrix) {
         vector<int> zigzag;
+        int m = matrix.size(), n = matrix[0].size();
+        
+        for (int i = 0; i < m + n - 1; ++i) {
+            if (i % 2) {
+                for (int y = min(i, n - 1);  y >= max(0, i - m + 1); --y) {
+                    zigzag.emplace_back(matrix[i - y][y]);
+                }
+            }
+            else {
+                for (int x = min(i, m - 1);  x >= max(0, i - n + 1) ; --x) {
+                    zigzag.emplace_back(matrix[x][i - x]);
+                }
+            }
+        }
+        
+        return zigzag;
+    }
+};
+
+// Time:  O((m + n)^2)
+// Space: O(1)
+
+class Solution2 {
+public:
+    /**
+     * @param matrix: a matrix of integers
+     * @return: a vector of integers
+     */
+    vector<int> printZMatrix(vector<vector<int> > &matrix) {
+        vector<int> zigzag;
         size_t m = matrix.size(), n = matrix[0].size();
         
         for (int i = 0; i < m + n - 1; ++i) {
