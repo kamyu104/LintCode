06cdc66dee43b45a12cd5599e8ed1ca1d749966e
Allen Liu <kamyu104@gmail.com>
2015-04-27 03:25:06 +0800

    update

---
+++ C++/wood-cut.cpp
@@ -0,0 +1,41 @@
+// Time:  O(nlogL)
+// Space: O(1)
+
+class Solution {
+public:
+    /**
+     *@param L: Given n pieces of wood with length L[i]
+     *@param k: An integer
+     *return: The maximum length of the small pieces.
+     */
+    int woodCut(vector<int> L, int k) {
+        int n = L.size();
+        if(n == 0) {
+            return 0;
+        }
+        
+        int left = 1, right = *max_element(L.begin(), L.end());
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            // Find the smallest x,  s.t. pieceCount(x) < k <= pieceCound(x - 1)
+            if (pieceCount(L, mid) < k) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        
+        // The max length is x - 1
+        return left - 1;
+    }
+    
+    int pieceCount(vector<int>& L, int x) {
+        int cnt = 0;
+        for (auto& len : L) {
+            cnt += len / x;
+        }
+        return cnt;
+    }
+};
--- README.md
+++ README.md
@@ -147,6 +147,7 @@ Algorithms
 |74|[First Bad Version](http://lintcode.com/en/problem/first-bad-version/)| [C++](./C++/first-bad-version.cpp)| _O(logn)_ | _O(1)_ | Medium | | |
 |75|[Find Peak Element](http://lintcode.com/en/problem/find-peak-element/)| [C++](./C++/find-peak-element.cpp)| _O(logn)_ | _O(1)_ | Medium | LeetCode | |
 |76|[Longest Increasing Subsequence](http://lintcode.com/en/problem/longest-increasing-subsequence/)| [C++](./C++/longest-increasing-subsequence.cpp)| _O(nlogn)_ | _O(n)_ | Medium | CC150 | |
+|183|[Wood Cut](http://lintcode.com/en/problem/wood-cut/)| [C++](./C++/wood-cut.cpp)| _O(nlogL)_ | _O(1)_ | Medium | | |
 
 ---
 
