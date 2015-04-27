5f51331c360f98c16c44d4244e089d84da030634
Allen Liu <kamyu104@gmail.com>
2015-04-28 01:27:26 +0800

    update

--- C++/trapping-rain-water-ii.cpp
+++ C++/trapping-rain-water-ii.cpp
@@ -1,86 +1,75 @@
+// Time:  O(n^2 * logn)
+// Space: O(n^2)
 
-vector<vector<int> > Visited;
-int HowMuchCanHold(vector<vector<int> > &M,int i,int j)
-{
-    Visited[i][j]=1;
-    if(i==0 || j==0 || i==M.size()-1 || j==M[0].size()-1) // we are in borders
-        return -1; // drops to earth
-    int up=M[i-1][j],down=M[i+1][j],left=M[i][j-1],right=M[i][j+1];
-    int minall=1<<10;
-    if(!Visited[i-1][j])
-        minall=min(minall,up);
-    if(!Visited[i+1][j])
-        minall=min(minall,down);
-    if(!Visited[i][j-1])
-        minall=min(minall,left);
-    if(!Visited[i][j+1])
-        minall=min(minall,right);
+// BFS with priority queue (min heap)
+class Solution {
+public:
+    struct cell {
+        int i;
+        int j;
+        int height;
+    };
     
-    int mij=M[i][j];
-    if(minall>mij)
-        return minall-mij;
-    else
-        if(minall<mij)
-            return -1; // can't hold the drop
-    
-    int minup=1<<10,mindown=1<<10,minleft=1<<10,minright=1<<10;
-    if(mij==up && Visited[i-1][j]==0) // drop may flow up
-        minup=HowMuchCanHold(M,i-1,j);
-    else
-        if(mij!=up)
-            minup=up-mij;
-    if(mij==down && Visited[i+1][j]==0) // drop may flow down
-        mindown=HowMuchCanHold(M,i+1,j);
-    else
-        if(mij!=down)
-            mindown=down-mij;
-    if(mij==left && Visited[i][j-1]==0) // drop may flow left
-        minleft=HowMuchCanHold(M,i,j-1);
-    else
-        if(mij!=left)
-            minleft=left-mij;
-    if(mij==right && Visited[i][j+1]==0) // drop may flow right
-        minright=HowMuchCanHold(M,i,j+1);
-    else
-        if(mij!=right)
-            minright=right-mij;
-    if(minup<0 || mindown<0 || minleft<0 || minright <0) // drop wil fall from up,down,left or right!
-        return -1; // can't hold the drop
-    int min1=min(minup,mindown);
-    int min2=min(minleft,minright);
-    minall=min(min1,min2);
-    return minall;
-}
-int DropTo(vector<vector<int> > &M,int i,int j)
-{
-    Visited=vector<vector<int> >(M.size(), vector<int>(M[0].size(),0));
-    return HowMuchCanHold(M,i,j);
-}
-int get_watr_trap(vector<vector<int> > M)
-{
-    int row=M.size();
-    int col=M[0].size();
-    int total=0;
-    int nexthold=0;
-    bool find=false;
-    while(1)
-    {
-        find=false;
-        for(int i=0;i<row;i++)
-        {
-            for(int j=0;j<col;j++)
-            {
-                nexthold=DropTo(M,i,j);
-                if(nexthold>0)
-                {
-                    find=true;
-                    M[i][j]+=nexthold;//fill current cell up to the maxhold
-                    total+=nexthold;
-                }
+    /**
+     * @param heights: a matrix of integers
+     * @return: an integer
+     */
+    int trapRainWater(vector<vector<int> > &heights) {
+        int m = heights.size();
+        int n = heights[0].size();
+        int trap = 0;
+        vector<vector<bool>> is_visited(m, vector<bool>(n, false));
+        
+        // Use min heap to get the lowerest cell.
+        auto comp = [](cell a, cell b ) { return a.height > b.height; };
+        priority_queue<cell , vector<cell>, decltype(comp)> heap(comp);
+        
+        // Put the cells on the border into min heap.
+        for (int i = 0; i < m; ++i) {
+            heap.emplace(cell{i, 0, heights[i][0]});
+            heap.emplace(cell{i, n - 1, heights[i][n - 1]});
+        }
+        for (int j = 0; j < n; ++j) {
+            heap.emplace(cell{0, j, heights[0][j]});
+            heap.emplace(cell{m - 1, j, heights[m - 1][j]});
+        }
+        
+        // BFS with priority queue (min heap)
+        while(!heap.empty()) {
+            cell c = heap.top();
+            heap.pop();
+            is_visited[c.i][c.j] = true;
+            
+            // Up
+            if (c.i + 1 < m && !is_visited[c.i + 1][c.j]) {
+                is_visited[c.i + 1][c.j] = true;
+                trap += max(0, c.height - heights[c.i + 1][c.j]);
+                heap.emplace(cell{c.i + 1, c.j, max(c.height,
+                                                    heights[c.i + 1][c.j])});
+            }
+            // Down
+            if (c.i - 1 >= 0 && !is_visited[c.i - 1][c.j]) {
+                is_visited[c.i - 1][c.j] = true;
+                trap += max(0, c.height - heights[c.i - 1][c.j]);
+                heap.emplace(cell{c.i - 1, c.j, max(c.height,
+                                                    heights[c.i - 1][c.j])});
+            }
+            // Right
+            if (c.j + 1 < n && !is_visited[c.i][c.j + 1]) {
+                is_visited[c.i][c.j + 1] = true;
+                trap += max(0, c.height - heights[c.i][c.j + 1]);
+                heap.emplace(cell{c.i, c.j + 1, max(c.height,
+                                                    heights[c.i][c.j + 1])});
+            }
+            // Left
+            if (c.j - 1 >= 0 && !is_visited[c.i][c.j - 1]) {
+                is_visited[c.i][c.j - 1] = true;
+                trap += max(0, c.height - heights[c.i][c.j - 1]);
+                heap.emplace(cell{c.i, c.j - 1, max(c.height,
+                                                    heights[c.i][c.j - 1])});
             }
         }
-        if(!find)
-            break;
+        
+        return trap;
     }
-    return total;
-}
+};
