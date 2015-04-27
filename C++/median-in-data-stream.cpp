cea5a47f44dd6851dd0c1687f2c19cf45472f50c
Allen Liu <kamyu104@gmail.com>
2015-04-27 23:22:53 +0800

    update

--- C++/median-in-data-stream.cpp
+++ C++/median-in-data-stream.cpp
@@ -1,8 +1,47 @@
 // Time:  O(nlogn)
 // Space: O(n)
 
-
 class Solution {
+public:
+    /**
+     * @param nums: A list of integers.
+     * @return: The median of numbers
+     */
+    vector<int> medianII(vector<int> &nums) {
+        // min_heap stores the larger half seen so far.
+        multiset<int, less<int>> min_bst;
+        // max_heap stores the smaller half seen so far.
+        multiset<int, greater<int>> max_bst;
+        
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); ++i) {
+            
+            // Balance smaller half and larger half.
+            if (max_bst.empty() || nums[i] > *max_bst.begin()) {
+                min_bst.insert(nums[i]);
+                if (min_bst.size() > max_bst.size() + 1) {
+                    max_bst.insert(*min_bst.begin());
+                    min_bst.erase(min_bst.begin());
+                }
+            }
+            else {
+                max_bst.insert(nums[i]);
+                if (max_bst.size() > min_bst.size()) {
+                    min_bst.insert(*max_bst.begin());
+                    max_bst.erase(max_bst.begin());
+                }
+            }
+            
+            ans.emplace_back(min_bst.size() == max_bst.size() ?
+                             *max_bst.begin() : *min_bst.begin());
+        }
+        
+        return ans;
+    }
+};
+
+
+class Solution2 {
 public:
     /**
      * @param nums: A list of integers.
