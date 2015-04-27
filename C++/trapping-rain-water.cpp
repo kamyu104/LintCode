3a23ea18bd71e940d4589018ca488100f44cca69
Allen Liu <kamyu104@gmail.com>
2015-04-28 01:55:36 +0800

    update

--- C++/trapping-rain-water.cpp
+++ C++/trapping-rain-water.cpp
@@ -1,6 +1,7 @@
 // Time:  O(n)
 // Space: O(1)
 
+// Two pointer solution
 class Solution {
 public:
     /**
@@ -32,6 +33,46 @@ public:
                 // Update current max height from right.
                 right_height = max(right_height, heights[j]);
             }
+        }
+        
+        return trap;
+    }
+};
+
+// Two pass scan solution
+class Solution2 {
+public:
+    /**
+     * @param heights: a vector of integers
+     * @return: a integer
+     */
+    int trapRainWater(vector<int> &heights) {
+        int trap = 0;
+        
+        // Find the top.
+        int top = 0;
+        for (int i = 0; i < heights.size(); ++i) {
+            if (heights[top] < heights[i]) {
+                top = i;
+            }
+        }
+        
+        // Count trapped water in the left of top.
+        int left_second_top = 0;
+        for (int i = 0; i < top; ++i) {
+            if (heights[left_second_top] < heights[i]) {
+                left_second_top = i;
+            }
+            trap += heights[left_second_top] - heights[i];
+        }
+        
+        // Count trapped water in the right of top.
+        int right_second_top = heights.size() - 1;
+        for (int i = heights.size() - 1; i > top; --i) {
+            if (heights[right_second_top] < heights[i]) {
+                right_second_top = i;
+            }
+            trap += heights[right_second_top] - heights[i];
         }
         
         return trap;
