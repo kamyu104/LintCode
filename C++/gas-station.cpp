9dffc505e94084bb1a79058a287b3a1bc24b6d68
Allen Liu <kamyu104@gmail.com>
2015-05-02 22:08:58 +0800

    update

---
+++ C++/gas-station.cpp
@@ -0,0 +1,37 @@
+// Time:  O(n)
+// Space: O(1)
+
+// Greedy solution.
+class Solution {
+public:
+    /**
+     * @param gas: a vector of integers
+     * @param cost: a vector of integers
+     * @return: an integer
+     */
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+        int current_sum = 0;
+        int total_sum = 0;
+        int start = 0;
+        for (int i = 0; i < gas.size(); ++i) {
+            current_sum += gas[i] - cost[i];
+            total_sum += gas[i] - cost[i];
+            if (current_sum < 0) {
+                // It means any gas station in range [start, i] cannot be start.
+                current_sum = 0;
+                start = i + 1;
+            }
+        }
+        
+        // Check if solution exists, i.e. sum(gas) >= sum(cost).
+        if (total_sum >= 0) {
+            // 1. route of [start, len(gas) - 1] must be positive
+            // 2. route of [0, start - 1] must be negative
+            // 3. sum(gas) >= sum(cost), so 1 + 2 >= 0
+            // According to the above, start is the gas station we want.
+            return start;
+        }
+        
+        return -1;
+    }
+};
