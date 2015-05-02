a34f860c5998413db7271b8a957f6abc763fb1d1
Allen Liu <kamyu104@gmail.com>
2015-05-02 18:10:04 +0800

    update

---
+++ C++/subarray-sum-closest.cpp
@@ -0,0 +1,45 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+class Solution {
+public:
+    /**
+     * @param nums: A list of integers
+     * @return: A list of integers includes the index of the first number
+     *          and the index of the last number
+     */
+    vector<int> subarraySumClosest(vector<int> nums){
+        auto n = nums.size();
+        
+        if(n == 1) {
+            return {0,0};
+        }
+        
+        // sum_from_start[i] denotes sum for 0 ~ i - 1.
+        vector<pair<int,int>> sum_from_start(n + 1, make_pair(0, 0));
+        sum_from_start[0].second = -1; // For case closest sum is from 0.
+        for (int  i = 0; i < n; ++i) {
+            sum_from_start[i+1].first = sum_from_start[i].first + nums[i];
+            sum_from_start[i+1].second = i;
+        }
+        
+        // Sort each sum from start.
+        std::sort(sum_from_start.begin(), sum_from_start.end());
+        
+        int min_diff = INT_MAX;
+        int start = -1;
+        int end = -1;
+        
+        // Find min difference of adjacent sum.
+        for (int i = 1; i <= n; ++i) {
+            int diff = abs(sum_from_start[i].first - sum_from_start[i-1].first);
+            if (diff < min_diff) {
+                min_diff = diff;
+                start = min(sum_from_start[i].second, sum_from_start[i - 1].second) + 1;
+                end = max(sum_from_start[i].second, sum_from_start[i - 1].second);
+            }
+        }
+        
+        return vector<int>{start, end};
+    }
+};
