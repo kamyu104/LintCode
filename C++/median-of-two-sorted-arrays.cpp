d27d4c629f7b04127645300e37958723742bb357
kamyu <kamyu104@users.noreply.github.com>
2015-05-04 00:54:39 +0800

    Update median-of-two-sorted-arrays.cpp

--- C++/median-of-two-sorted-arrays.cpp
+++ C++/median-of-two-sorted-arrays.cpp
@@ -29,7 +29,8 @@ public:
         
         int left = 0;
         int right = m;
-        // Find min left s.t. A[left] >= B[k - 1 - left]. left is the (k + 1)-th element.
+        // Find a partition of A and B
+        // where min left s.t. A[left] >= B[k - 1 - left]. Thus left is the (k + 1)-th element.
         while (left < right) {
             int mid = left + (right - left) / 2;
             if (0 <= k - 1 - mid && k - 1 - mid < n && A[mid] >= B[k - 1 - mid]) {
