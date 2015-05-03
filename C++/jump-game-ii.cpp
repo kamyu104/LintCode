c3dd008449fefa809e8b8e7ded477a81c5990d18
Allen Liu <kamyu104@gmail.com>
2015-05-03 22:31:26 +0800

    update

---
+++ C++/jump-game-ii.cpp
@@ -0,0 +1,30 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    /**
+     * @param A: A list of lists of integers
+     * @return: An integer
+     */
+    int jump(vector<int> A) {
+        int jump_count = 0;
+        int reachable = 0;
+        int curr_reachable = 0;
+        for (int i = 0; i < A.size(); ++i) {
+            if (i > reachable) {
+                return -1;
+            }
+            if (i > curr_reachable) {
+                // current jumps are not enough, jump one more.
+                curr_reachable = reachable;
+                ++jump_count;
+            }
+            reachable = max(reachable, i + A[i]);
+        }
+        
+        return jump_count;
+    }
+};
+
+
---
+++ C++/jump-game.cpp
@@ -0,0 +1,22 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    /**
+     * @param A: A list of integers
+     * @return: The boolean answer
+     */
+    bool canJump(vector<int> A) {
+        int reachable = 0;
+        for (int i = 0; i < A.size(); ++i) {
+            if (i > reachable) {
+                return false;
+            }
+            reachable = max(reachable, i + A[i]);
+        }
+        
+        return true;
+    }
+};
+
