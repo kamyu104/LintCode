09776991d57af115ac7401f081089531ec6975f0
kamyu <kamyu104@users.noreply.github.com>
2015-05-04 15:01:13 +0800

    Update 4-sum.cpp

--- C++/4-sum.cpp
+++ C++/4-sum.cpp
@@ -10,7 +10,6 @@ public:
      *          zero.
      */
     vector<vector<int> > fourSum(vector<int> nums, int target) {
-        
         sort(nums.begin(), nums.end()); // Make sure quadruplets are in non-descending orders.
         unordered_map<int, vector<vector<size_t>>> two_sum; // two_sum saves "sum to (i, j) pairs, which i < j."
         for (size_t i = 0; i < nums.size(); ++i) {
