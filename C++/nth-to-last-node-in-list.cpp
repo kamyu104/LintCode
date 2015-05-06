30fcc60d3214ba8835e584106011f0be727f64dc
Allen Liu <kamyu104@gmail.com>
2015-05-07 01:17:55 +0800

    update

---
+++ C++/nth-to-last-node-in-list.cpp
@@ -0,0 +1,44 @@
+// Time:  O(n), n is lengh of the linked list.
+// Space: O(1)
+
+/**
+ * Definition of ListNode
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param head: The first node of linked list.
+     * @param n: An integer.
+     * @return: Nth to last node of a singly linked list.
+     */
+    ListNode *nthToLast(ListNode *head, int n) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        
+        // fast is n-step ahead.
+        while (n > 0) {
+            fast = fast->next;
+            --n;
+        }
+        
+        // When fast reaches the end, slow must be nth to last node.
+        while (fast != nullptr) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        
+        return slow;
+    }
+};
+
+
+
