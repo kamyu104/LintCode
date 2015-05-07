d133bf1ff942c79d39770c6e0431a16964fd8aa6
Allen Liu <kamyu104@gmail.com>
2015-05-08 00:47:11 +0800

    update

---
+++ C++/reverse-words-in-a-string.cpp
@@ -0,0 +1,30 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    /**
+     * @param s : A string
+     * @return : A string
+     */
+    string reverseWords(string s) {
+        // Reverse the whole string first.
+        reverse(s.begin(), s.end());
+        
+        size_t start = 0, end;
+        while ((end = s.find(" ", start)) != string::npos) {
+            // Reverse each word in the string.
+            reverse(s.begin() + start, s.begin() + end);
+            start = end + 1;
+        }
+        // Reverse the last word.
+        reverse(s.begin() + start, s.end());
+        
+        // Remove beginning blank.
+        if ((start = s.find_first_not_of(" ")) != string::npos) {
+            return s.substr(start);
+        }
+        
+        return s;
+    }
+};
