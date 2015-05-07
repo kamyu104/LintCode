436b0e7f4c75bec210f52e43f6de4d9e9e0464e3
Allen Liu <kamyu104@gmail.com>
2015-05-08 01:03:45 +0800

    update

---
+++ C++/insertion-sort-list.cpp
@@ -0,0 +1,46 @@
+// Time:  O(n^2)
+// Space: O(1)
+
+/**
+ * Definition of ListNode
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param head: The first node of linked list.
+     * @return: The head of linked list.
+     */
+    ListNode *insertionSortList(ListNode *head) {
+        ListNode dummy(INT_MIN);
+        
+        ListNode *cur = head;
+        ListNode *prev = nullptr;
+        ListNode *pos = nullptr;
+        
+        while (cur) {
+            pos = findInsertPos(&dummy, cur->val);
+            ListNode *tmp = cur->next;
+            cur->next = pos->next;
+            pos->next = cur;
+            cur = tmp;
+        }
+        
+        return dummy.next;
+    }
+    
+    ListNode* findInsertPos(ListNode *head, int x) {
+        ListNode *pre = NULL;
+        for (ListNode *cur = head; cur && cur->val <= x;
+             pre = cur, cur = cur->next);
+        return pre;
+    }
+};
