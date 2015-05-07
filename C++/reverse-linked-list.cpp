30ed05082601fca092d4ff448ec8fb9253d29d95
Allen Liu <kamyu104@gmail.com>
2015-05-08 03:25:37 +0800

    update

---
+++ C++/reverse-linked-list.cpp
@@ -0,0 +1,37 @@
+// Time:  O(n)
+// Space: O(1)
+
+/**
+ * Definition of ListNode
+ *
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ *
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param head: The first node of linked list.
+     * @return: The new head of reversed linked list.
+     */
+    ListNode *reverse(ListNode *head) {
+        ListNode *dummy_head = new ListNode(INT_MIN);
+        
+        while (head != nullptr) {
+            ListNode *tmp = head->next;
+            head->next = dummy_head->next;
+            dummy_head->next = head;
+            head = tmp;
+        }
+        
+        return dummy_head->next;
+    }
+};
+
