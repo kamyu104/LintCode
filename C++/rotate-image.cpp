d3d8e7b392e8b7a2ebc7429ec313ac74646270b7
Allen Liu <kamyu104@gmail.com>
2015-05-09 13:50:40 +0800

    update

---
+++ C++/rotate-image.cpp
@@ -0,0 +1,22 @@
+// Time:  O(n^2)
+// Space: O(1)
+
+class Solution {
+public:
+    /**
+     * @param matrix: A list of lists of integers
+     * @return: Void
+     */
+    void rotate(vector<vector<int> > &matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = i; j < n - 1 - i; ++j) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1- j][i] = matrix[n-1-i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = tmp;
+            }
+        }
+    }
+};
