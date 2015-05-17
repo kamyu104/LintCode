622558ffc830384c0ec5dd74e62d0e8f8bc051b2
Allen Liu <kamyu104@gmail.com>
2015-05-18 02:57:58 +0800

    update

--- C++/maximum-subarray-iii.cpp
+++ C++/maximum-subarray-iii.cpp
@@ -9,7 +9,7 @@ public:
      * @return: An integer denote the sum of max k non-overlapping subarrays
      */
     int maxSubArray(vector<int> nums, int k) {
-        int n = nums.size();
+        const int n = nums.size();
         
         // sums[x][y] means the max sum in range [0, x - 1] with k non-overlapping subarrays
         vector<vector<int>> sums(n + 1, vector<int>(k + 1, INT_MIN));
@@ -18,11 +18,11 @@ public:
             sums[i][0] = 0;
         }
         
-        for(int i = 1; i <= n; ++i) {
-            for(int j = 1; j <= min(i, k); ++j) {
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= min(i, k); ++j) {
                 sums[i][j] = sums[i - 1][j];
-                int sum_p_i = 0;
-                for(int p = i; p > j - 1; --p) {
+                int max_sum_from_p = 0;
+                for (int p = i; p > j - 1; --p) {
                     max_sum_from_p = max(0, max_sum_from_p) + nums[p - 1];
                     // max sum in range[0, i - 1] with j subarrays equals to
                     // max sum in max(range [0, p - 2] with j - 1 subarrys plus
@@ -47,20 +47,20 @@ public:
      * @return: An integer denote the sum of max k non-overlapping subarrays
      */
     int maxSubArray(vector<int> nums, int k) {
-        int n = nums.size();
+        const int n = nums.size();
         
         // sums[x][y] means the max sum in range [0, x - 1] with k non-overlapping subarrays
         vector<vector<int>> sums(n + 1, vector<int>(k + 1, INT_MIN));
         
-        for(int i = 0; i <= n; ++i) {
+        for (int i = 0; i <= n; ++i) {
             sums[i][0] = 0;
         }
         
-        for(int i = 1; i <= n; ++i) {
-            for(int j = 1; j <= min(i, k); ++j) {
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= min(i, k); ++j) {
                 sums[i][j] = sums[i - 1][j];
                 int sum = 0, max_sum = INT_MIN;
-                for(int p = i; p > j - 1; --p) {
+                for (int p = i; p > j - 1; --p) {
                     sum += nums[p - 1];
                     max_sum = max(max_sum, sum);
                     sum = max(0, sum);
