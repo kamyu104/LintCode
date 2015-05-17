a859001508c5cfb9e263ddb88ed0adeebed24a94
Allen Liu <kamyu104@gmail.com>
2015-05-18 05:27:22 +0800

    update

--- C++/single-number-iii.cpp
+++ C++/single-number-iii.cpp
@@ -15,7 +15,7 @@ public:
         }
         
         // Get the last bit where 1 occurs.
-        auto bit = x_xor_y & ~(x_xor_y - 1);
+        const auto bit = x_xor_y & ~(x_xor_y - 1);
         
         // Get the subset of A where the number has the bit.
         // The subset only contains one of the two integers, call it x.
