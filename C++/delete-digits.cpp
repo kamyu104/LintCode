ff3faecca278c3731f7ffa8eff4b10039eb7c8f6
Allen Liu <kamyu104@gmail.com>
2015-05-18 02:24:34 +0800

    update

--- C++/delete-digits.cpp
+++ C++/delete-digits.cpp
@@ -66,53 +66,56 @@ public:
     }
 };
 
-// Time:  O(kn)
+// Time:  O(k * n)
 // Space: O(1)
 // Greedy method
 class Solution2 {
-    public:
-        /**
-         *@param A: A positive integer which has N digits, A is a string.
-         *@param k: Remove k digits.
-         *@return: A string
-         */
-        string DeleteDigits(string A, int k) {
-            auto len = A.size();
-
-            // Handle boundary case
-            if (len == k) {
-                return "0";
+public:
+    /**
+     *@param A: A positive integer which has N digits, A is a string.
+     *@param k: Remove k digits.
+     *@return: A string
+     */
+    string DeleteDigits(string A, int k) {
+        auto len = A.size();
+        
+        // Handle boundary case
+        if (len == k) {
+            return "0";
+        }
+        
+        // If a digit is greater than next one, delete it.
+        int i = 0;
+        while (i + 1 <= len && k > 0) {
+            if (A[i] > A[i + 1]) {
+                A.erase(A.begin() + i); // Not efficient, linear time
+                i = max(0, i - 1);
+                --k;
+            } else {
+                ++i;
             }
-
-            // If a digit is greater than next one, delete it.
-            int i = 0;
-            while (i + 1 <= len && k > 0) {
-                if (A[i] > A[i + 1]) {
-                    A.erase(A.begin() + i); // Not efficient, linear time
-                    i = max(0, i - 1);
-                    --k;
-                } else {
-                    ++i;
-                }
+        }
+        
+        // If all digits are increasingly sorted, delete last.
+        if (k > 0) {
+            A = A.substr(0, A.length() - k);
+        }
+        
+        // Strip all leading '0'
+        if (A.length() > 0 && A[0] == '0') {
+            size_t pos = A.find_first_not_of("0");
+            if (pos != string::npos) {
+                A = A.substr(A.find_first_not_of("0"));
             }
-
-            // If all digits are increasingly sorted, delete last.
-            if (k > 0) {
-                A = A.substr(0, A.length() - k);
-            }
-
-            // Strip all leading '0' 
-            while(A.length() > 0 && A[0] == '0') {
-                A.erase(A.begin()); // Not efficient, linear time
-            }
-
-            // Handle boundary case
-            if (A.length() == 0) {
-                return "0";
-            }
-
-            return A;
-        } 
+        }
+        
+        // Handle boundary case
+        if (A.length() == 0) {
+            return "0";
+        }
+        
+        return A;
+    }
 };
 
 
