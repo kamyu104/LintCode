73c61f52f7b421ef53ae30e56f3365d9a171f7b5
kamyu <kamyu104@users.noreply.github.com>
2015-05-18 02:29:50 +0800

    Update single-number-iii.cpp

--- C++/single-number-iii.cpp
+++ C++/single-number-iii.cpp
@@ -10,7 +10,7 @@ public:
     vector<int> singleNumberIII(vector<int> &A) {
         // Xor all the elements to get x ^ y.
         int x_xor_y = 0;
-        for (auto& i : A) {
+        for (const auto& i : A) {
             x_xor_y ^= i;
         }
         
@@ -21,7 +21,7 @@ public:
         // The subset only contains one of the two integers, call it x.
         // Xor all the elemets in the subset to get x.
         int x = 0;
-        for (auto& i : A) {
+        for (const auto& i : A) {
             if (i & bit) {
                 x ^= i;
             }
