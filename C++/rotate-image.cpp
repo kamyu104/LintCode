c8211c41f582a85e8fce3b7eaa8b5fda27008975
Allen Liu <kamyu104@gmail.com>
2015-05-17 13:49:09 +0800

    update

--- C++/rotate-image.cpp
+++ C++/rotate-image.cpp
@@ -20,3 +20,26 @@ public:
         }
     }
 };
+
+class Solution2 {
+public:
+    /**
+     * @param matrix: A list of lists of integers
+     * @return: Void
+     */
+    void rotate(vector<vector<int> > &matrix) {
+        int n = matrix.size();
+        // Anti-diagonal mirror.
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n - i; ++j) {
+                swap(matrix[i][j], matrix[n - 1 - j][n - 1 - i]);
+            }
+        }
+        // Horizontal mirror.
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < n; ++j) {
+                swap(matrix[i][j], matrix[n - 1 - i][j]);
+            }
+        }
+    }
+};
