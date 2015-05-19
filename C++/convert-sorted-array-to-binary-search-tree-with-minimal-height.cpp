57c961e13cecbc23584f576200d0665fabdd836b
kamyu <kamyu104@users.noreply.github.com>
2015-05-19 14:40:56 +0800

    Update convert-sorted-array-to-binary-search-tree-with-minimal-height.cpp

--- C++/convert-sorted-array-to-binary-search-tree-with-minimal-height.cpp
+++ C++/convert-sorted-array-to-binary-search-tree-with-minimal-height.cpp
@@ -34,18 +34,6 @@ public:
     }
 };
 
-/**
- * Definition of TreeNode:
- * class TreeNode {
- * public:
- *     int val;
- *     TreeNode *left, *right;
- *     TreeNode(int val) {
- *         this->val = val;
- *         this->left = this->right = NULL;
- *     }
- * }
- */
 class Solution2 {
 public:
     /**
