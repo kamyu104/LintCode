53be9705a9c1f78e3248a1b6a22a624a1d2dba32
kamyu <kamyu104@users.noreply.github.com>
2015-05-19 14:17:16 +0800

    Update triangle-count.cpp

--- C++/triangle-count.cpp
+++ C++/triangle-count.cpp
@@ -1,6 +1,37 @@
-// Time:  O(n^3)
+// Time:  O(n^2)
 // Space: O(1)
 
+// Two Pointers solution.
+class Solution {
+public:
+    /**
+     * @param S: A list of integers
+     * @return: An integer
+     */
+    int triangleCount(vector<int> &S) {
+        sort(S.begin(), S.end());
+        const int n = S.size();
+        
+        int count = 0;
+        for (int k = n - 1; k > 1; --k) {
+            int i = 0, j = k - 1;
+            while (i < j) { // Two Pointers, linear time.
+                if (S[i] + S[j] <= S[k]) {
+                    ++i;
+                } else {
+                    count += j - i;
+                    --j;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
+
+
+// Time:  O(n^3)
+// Space: O(1)
 class Solution2 {
 public:
     /**
@@ -14,11 +45,8 @@ public:
         int count = 0;
         
         for (int i = 0; i < n - 2; ++i) {
-            // Initialize index of the rightmost third element
             int k = i + 2;
-            
-            // Fix the second element
-            for (int j = i+1; j < n; ++j) {
+            for (int j = i + 1; j < n; ++j) {
                 while (k < n && S[i] + S[j] > S[k]) {
                     ++k;
                 }
