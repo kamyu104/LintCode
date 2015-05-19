1c4837f5909a32e4c0d89d3f02f2d80736d2b830
kamyu <kamyu104@users.noreply.github.com>
2015-05-19 14:26:14 +0800

    Create plus-one.cpp

---
+++ C++/plus-one.cpp
@@ -0,0 +1,23 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    /**
+     * @param digits a number represented as an array of digits
+     * @return the result
+     */
+    vector<int> plusOne(vector<int>& digits) {
+        vector<int> result(digits.cbegin(), digits.cend());
+        int carry = 1;
+        for (auto it = result.rbegin(); it != result.rend(); ++it) {
+            *it += carry;
+            carry = *it / 10;
+            *it %= 10;
+        }
+        if (carry == 1) {
+            result.insert(result.begin(), carry);
+        }
+        return result;
+    }
+};
