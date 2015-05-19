3b4ca2770d974461b096f5656b492af4e63cc9a4
kamyu <kamyu104@users.noreply.github.com>
2015-05-19 14:39:05 +0800

    Create convert-sorted-array-to-binary-search-tree-with-minimal-height.cpp

---
+++ C++/convert-sorted-array-to-binary-search-tree-with-minimal-height.cpp
@@ -0,0 +1,38 @@
+// Time:  O(n)
+// Space: O(logn)
+
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param A: A sorted (increasing order) array
+     * @return: A tree node
+     */
+    TreeNode *sortedArrayToBST(vector<int> &A) {
+        return sortedArrayToBSTHelper(A, 0, A.size() - 1);
+    }
+    
+    TreeNode *sortedArrayToBSTHelper(vector<int> &A, int start, int end) {
+        if (start <= end) {
+            TreeNode *node = new TreeNode(A[start + (end - start) / 2]);
+            node->left = sortedArrayToBSTHelper(A, start, start + (end - start) / 2 - 1);
+            node->right = sortedArrayToBSTHelper(A, start + (end - start) / 2 + 1, end);
+            return node;
+        }
+        return nullptr;
+    }
+};
+
+
+
