6fa6f48a1170f154f71189fa58b15df1f07b67a9
kamyu <kamyu104@users.noreply.github.com>
2015-05-23 01:10:25 +0800

    Update coins-in-a-line-iii.cpp

--- C++/coins-in-a-line-iii.cpp
+++ C++/coins-in-a-line-iii.cpp
@@ -1,7 +1,121 @@
 // Time:  O(n^2)
-// Space: O(n^2)
+// Space: O(n)
 
 class Solution {
+public:
+    /**
+     * @param values: a vector of integers
+     * @return: a boolean which equals to true if the first player will win
+     */
+    bool firstWillWin(vector<int> &values) {
+        // Even number case;
+        if (values.size() % 2 == 0) {
+            if (firstWinEvenCoins(values)) {
+                return true;
+            }
+        }
+        
+        // Count total.
+        int sum = 0;
+        for (int i = 0; i < values.size(); ++i) {
+            sum += values[i];
+        }
+        
+        // P[i][n] = max(values[i] + min(P[i + 2][n - 2], P[i + 1][n - 2]),
+        //               values[j] + min(P[i + 1][n - 2], P[i][n - 2]))
+        vector<vector<int>> P(values.size(), vector<int>(3, 0));
+
+        for (int n = 0; n <= values.size(); ++n) {
+            for (int i = 0; i + n - 1 < values.size(); ++i) {
+                int a = i + 2 <= values.size() - 1 && n - 2 >= 0 ? P[i + 2][(n - 2) % 3] : 0;
+                int b = i + 1 <= values.size() - 1 && n - 2 >= 0 ? P[i + 1][(n - 2) % 3] : 0;
+                int c = n - 2 >= 0 ? P[i][(n - 2) % 3] : 0;
+                P[i][n % 3] = max(values[i] + min(a, b),
+                              values[i + n - 1] + min(b, c));
+            }
+        }
+        return P[0][values.size() % 3] > sum - P[0][values.size() % 3];
+    }
+    
+    // Time: O(n), Space: O(1)
+    // Odd / even numbered strategy for even numbered coins
+    bool firstWinEvenCoins(const vector<int>& values) {
+        int sum = 0;
+        int odd_sum = 0;
+        int even_sum = 0;
+        for (int i = 0; i < values.size(); ++i) {
+            sum += values[i];
+            if (i % 2 == 0) {
+                odd_sum += values[i];
+            } else {
+                even_sum += values[i];
+            }
+        }
+        
+        return odd_sum != even_sum;
+    }
+};
+
+// Time:  O(n^2)
+// Space: O(n^2)
+class Solution2 {
+public:
+    /**
+     * @param values: a vector of integers
+     * @return: a boolean which equals to true if the first player will win
+     */
+    bool firstWillWin(vector<int> &values) {
+        // Even number case;
+        if (values.size() % 2 == 0) {
+            if (firstWinEvenCoins(values)) {
+                return true;
+            }
+        }
+        
+        // Count total.
+        int sum = 0;
+        for (int i = 0; i < values.size(); ++i) {
+            sum += values[i];
+        }
+        
+        // P[i][n] = max(values[i] + min(P[i + 2][n - 2], P[i + 1][n - 2]),
+        //               values[j] + min(P[i + 1][n - 2], P[i][n - 2]))
+        vector<vector<int>> P(values.size(), vector<int>(values.size() + 1, 0));
+
+        for (int n = 0; n <= values.size(); ++n) {
+            for (int i = 0; i + n - 1 < values.size(); ++i) {
+                int a = i + 2 <= values.size() - 1 && n - 2 >= 0 ? P[i + 2][n - 2] : 0;
+                int b = i + 1 <= values.size() - 1 && n - 2 >= 0 ? P[i + 1][n - 2] : 0;
+                int c = n - 2 >= 0 ? P[i][n - 2] : 0;
+                P[i][n] = max(values[i] + min(a, b),
+                              values[i + n - 1] + min(b, c));
+            }
+        }
+        return P[0][values.size()] > sum - P[0][values.size()];
+    }
+    
+    // Time: O(n), Space: O(1)
+    // Odd / even numbered strategy for even numbered coins
+    bool firstWinEvenCoins(const vector<int>& values) {
+        int sum = 0;
+        int odd_sum = 0;
+        int even_sum = 0;
+        for (int i = 0; i < values.size(); ++i) {
+            sum += values[i];
+            if (i % 2 == 0) {
+                odd_sum += values[i];
+            } else {
+                even_sum += values[i];
+            }
+        }
+        
+        return odd_sum != even_sum;
+    }
+};
+
+// Time:  O(n^2)
+// Space: O(n^2)
+class Solution3 {
 public:
     /**
      * @param values: a vector of integers
