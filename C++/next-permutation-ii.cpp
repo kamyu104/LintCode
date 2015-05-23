ee34e900666378ca95c1f4b9f2c26254febe9a85
kamyu <kamyu104@users.noreply.github.com>
2015-05-23 11:50:09 +0800

    Create next-permutation-ii.cpp

---
+++ C++/next-permutation-ii.cpp
@@ -0,0 +1,28 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    /**
+     * @param nums: a vector of integers
+     * @return: return nothing (void), do not return anything, modify nums in-place instead
+     */
+    void nextPermutation(vector<int> &nums) {
+        int k = -1, l = 0;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            if (nums[i] < nums[i + 1]) {
+                k = i;
+            }
+        }
+        
+        if (k >= 0) {
+            for (int i = 0; i < nums.size(); ++i) {
+                if (nums[i] > nums[k]) {
+                    l = i;
+                }
+            }
+            swap(nums[k], nums[l]);
+        }
+        reverse(nums.begin() + k + 1, nums.end());
+    }
+};
