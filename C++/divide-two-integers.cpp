5e56eb2a05ce932bbde514a51934df330ebd8c94
kamyu <kamyu104@users.noreply.github.com>
2015-05-24 10:34:49 +0800

    Update divide-two-integers.cpp

--- C++/divide-two-integers.cpp
+++ C++/divide-two-integers.cpp
@@ -25,6 +25,55 @@ public:
         if (divisor > 0) {
             divisor = -divisor;
         }
+        
+        int product = divisor;
+        int idx = 1;
+        while (product < 0 && idx < 32) {
+            ++idx;
+            product <<= 1;
+        }
+        idx -= 2; // Skip value of INT_MIN
+        product = divisor << idx;
+        int multiplier = 1 << idx;
+        
+        int res = 0;
+        while (dividend <= divisor) {
+            if (dividend <= product) {
+                res += multiplier;
+                dividend -= product;
+            }
+            --idx;
+            product = divisor << idx;
+            multiplier >>= 1;
+        }
+        return negative ? -res : res;
+    }
+};
+
+class Solution2 {
+public:
+    /**
+     * @param dividend the dividend
+     * @param divisor the divisor
+     * @return the result
+     */
+    int divide(int dividend, int divisor) {
+        // Handle corner case.
+        if (divisor == INT_MIN) {
+            return dividend == divisor ? 1 : 0;
+        } else if (!divisor || divisor == -1 && dividend == INT_MIN) {
+            return INT_MAX;
+        } else if (divisor == 1) {
+            return dividend;
+        }
+        
+        bool negative = (dividend > 0) ^ (divisor > 0);
+        if (dividend > 0) {
+            dividend = -dividend;
+        }
+        if (divisor > 0) {
+            divisor = -divisor;
+        }
         vector<int> multipliers, products;
         multipliers.push_back(1);
         products.push_back(divisor);
