4455f18c3f725d7d9fe16f6dcd4cbf027266516b
kamyu <kamyu104@users.noreply.github.com>
2015-05-24 11:20:58 +0800

    Create candy.cpp

---
+++ C++/candy.cpp
@@ -0,0 +1,26 @@
+// Time:  O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    /**
+     * @param ratings Children's ratings
+     * @return the minimum candies you must give
+     */
+    int candy(vector<int>& ratings) {
+        vector<int> candies(ratings.size(), 1);
+        for (int i = 1; i < ratings.size(); ++i) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        
+        for (int i = ratings.size(); i >= 0; --i) {
+            if (ratings[i - 1] > ratings[i] && candies[i - 1] <= candies[i]) {
+                candies[i - 1] = candies[i] + 1;
+            }
+        }
+        
+        return accumulate(candies.cbegin(), candies.cend(), 0);
+    }
+};
