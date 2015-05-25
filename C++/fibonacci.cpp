5d1f12a1243d961cb8780c253de24b4953f39033
kamyu <kamyu104@users.noreply.github.com>
2015-05-26 01:34:05 +0800

    Create fibonacci.cpp

---
+++ C++/fibonacci.cpp
@@ -0,0 +1,25 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution{
+public:
+    /**
+     * @param n: an integer
+     * @return an integer f(n)
+     */
+    int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        
+        int n_i = 1, n_i_1 = 1, n_i_2 = 0;
+        for (int i = 2; i < n; ++i) {
+            n_i = n_i_1 + n_i_2;
+            n_i_2 = n_i_1;
+            n_i_1 = n_i;
+        }
+        
+        return n_i;
+    }
+};
+
