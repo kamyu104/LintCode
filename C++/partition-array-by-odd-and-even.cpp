d7e5d74041d83224fb37bed357b10a938272201e
kamyu <kamyu104@users.noreply.github.com>
2015-05-26 00:12:37 +0800

    Create partition-array-by-odd-and-even.cpp

---
+++ C++/partition-array-by-odd-and-even.cpp
@@ -0,0 +1,26 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    /**
+     * @param nums: a vector of integers
+     * @return: nothing
+     */
+    void partitionArray(vector<int> &nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left <= right) {
+            while (left <= right && nums[left] % 2) {
+                ++left;
+            } 
+            while (left <= right && nums[right] % 2 == 0) {
+                --right;
+            }
+            if (left <= right) {
+               swap(nums[left++], nums[right--]);
+            }
+        }
+    }
+};
