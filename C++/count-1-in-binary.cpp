5a01520d92802f432902aaf3715f731008bffbc1
kamyu <kamyu104@users.noreply.github.com>
2015-05-26 01:40:06 +0800

    Create count-1-in-binary.cpp

---
+++ C++/count-1-in-binary.cpp
@@ -0,0 +1,17 @@
+// Time:  O(m)
+// Space: O(1)
+
+class Solution {
+public:
+    /**
+     * @param num: an integer
+     * @return: an integer, the number of ones in num
+     */
+    int countOnes(int num) {
+        int count = 0;
+        for(; num; num &= num - 1) {
+            ++count;
+        }
+        return count;
+    }
+};
