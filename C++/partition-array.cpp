bf90390a4d5fc9afddbf8ddd72013d078fc3f8c1
kamyu <kamyu104@users.noreply.github.com>
2015-05-26 00:11:16 +0800

    Update partition-array.cpp

--- C++/partition-array.cpp
+++ C++/partition-array.cpp
@@ -5,6 +5,28 @@ class Solution {
 public:
     int partitionArray(vector<int> &nums, int k) {
         int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left <= right) {
+            while (left <= right && nums[left] < k) {
+                ++left;
+            } 
+            while (left <= right && nums[right] >= k) {
+                --right;
+            }
+            if (left <= right) {
+               swap(nums[left++], nums[right--]);
+            }
+        }
+        
+        return left;
+    }
+};
+
+class Solution2 {
+public:
+    int partitionArray(vector<int> &nums, int k) {
+        int left = 0;
         int right = nums.size();
         
         while (left < right) {
