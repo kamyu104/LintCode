81b5ddbe3f9ff34468c8f338158a8952e7bccc76
kamyu <kamyu104@users.noreply.github.com>
2015-05-26 00:50:28 +0800

    Create subtree.cpp

---
+++ C++/subtree.cpp
@@ -0,0 +1,48 @@
+// Time:  O(m * n)
+// Space: O(h)
+
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param T1, T2: The roots of binary tree.
+     * @return: True if T2 is a subtree of T1, or false.
+     */
+    bool isSubtree(TreeNode *T1, TreeNode *T2) {
+        if (!T2) {
+            return true;
+        } else if (!T1) { // !T1 && T2
+            return false;
+        } else { // T1 && T2
+            return isSameTree(T1, T2) || 
+                   isSubtree(T1->left, T2) || 
+                   isSubtree(T1->right, T2);
+        }
+        
+    }
+    
+    bool isSameTree(TreeNode *T1, TreeNode *T2) {
+        if (!T1 && !T2) {
+            return true;
+        }
+        
+        if (T1 && T2) {
+            return T1->val == T2->val &&
+                   isSameTree(T1->left, T2->left) &&
+                   isSameTree(T1->right, T2->right);
+        }
+        
+        return false;
+    }
+};
