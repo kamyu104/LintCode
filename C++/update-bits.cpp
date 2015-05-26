c4b5d67d94cbbf354b68276250e96e2d58495ca3
kamyu <kamyu104@users.noreply.github.com>
2015-05-26 23:55:19 +0800

    Update update-bits.cpp

--- C++/update-bits.cpp
+++ C++/update-bits.cpp
@@ -10,7 +10,7 @@ public:
      */
     int updateBits(int n, int m, int i, int j) {
         int right_part = n & ((1 << i) - 1);
-        // The behavior of right shit >= 32 is undefined in C++.
+        // The behavior of right shift >= 32 is undefined in C++.
         int left_part = j >= 31 ? 0 : (n >> (j + 1)) << (j + 1);
         return left_part | (m << i) | right_part;
     }
