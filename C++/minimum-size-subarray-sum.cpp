0c861096302e26f2f89275e31c6da27d139ca1b6
kamyu <kamyu104@users.noreply.github.com>
2015-05-27 13:16:31 +0800

    Update minimum-size-subarray-sum.cpp

--- C++/minimum-size-subarray-sum.cpp
+++ C++/minimum-size-subarray-sum.cpp
@@ -17,7 +17,37 @@ public:
                 sum -= nums[++start];
             }
         }
-        if (start == -1) {
+        if (min_size == INT_MAX) {
+            return 0;
+        }
+        return min_size;
+    }
+};
+
+// Time:  O(nlogn)
+// Space: O(n)
+// Binary search solution.
+class Solution2 {
+public:
+    /**
+     * @param nums: a vector of integers
+     * @param s: an integer
+     * @return: an integer representing the minimum size of subarray
+     */
+    int minimumSize(vector<int> &nums, int s) {
+        int min_size = INT_MAX;
+        vector<int> sum_from_start(nums.size());
+        partial_sum(nums.begin(), nums.end(), sum_from_start.begin());
+        for (int i = 0; i < nums.size(); ++i) {
+            const auto& end_it = lower_bound(sum_from_start.begin() + i,
+                                          sum_from_start.end(),
+                                          sum_from_start[i] - nums[i] + s);
+            int end = static_cast<int>(end_it - sum_from_start.begin());
+            if (end_it != sum_from_start.end()) {
+                min_size = min(min_size,  end  - i + 1);
+            }
+        }
+        if (min_size == INT_MAX) {
             return 0;
         }
         return min_size;
