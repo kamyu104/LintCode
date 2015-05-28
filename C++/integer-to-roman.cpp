cf9d1c65023686c70f8caf844dc56ee19065a6f2
kamyu <kamyu104@users.noreply.github.com>
2015-05-28 13:05:19 +0800

    Update integer-to-roman.cpp

--- C++/integer-to-roman.cpp
+++ C++/integer-to-roman.cpp
@@ -8,9 +8,9 @@ public:
      * @return Roman representation
      */
     string intToRoman(int n) {
-        map<int, string, greater<int>> numeral_map = {{1, "I"}, {4, "IV"}, {5, "V"}, {9, "IX"}, 
-                                                      {10, "X"}, {40, "XL"}, {50, "L"}, {90, "XC"}, 
-                                                      {100, "C"}, {400, "CD"}, {500, "D"}, {900, "CM"}, 
+        map<int, string, greater<int>> numeral_map = {{1,    "I"}, {4,   "IV"}, {5,   "V"}, {9,   "IX"}, 
+                                                      {10,   "X"}, {40,  "XL"}, {50,  "L"}, {90,  "XC"}, 
+                                                      {100,  "C"}, {400, "CD"}, {500, "D"}, {900, "CM"}, 
                                                       {1000, "M"}};
         string result;
         while (n > 0) {
