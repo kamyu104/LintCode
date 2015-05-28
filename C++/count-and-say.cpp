ed7192c073a2197002ab7cdef6771f1e03849de7
kamyu <kamyu104@users.noreply.github.com>
2015-05-28 12:48:47 +0800

    Create count-and-say.cpp

---
+++ C++/count-and-say.cpp
@@ -0,0 +1,29 @@
+// Time:  O(n * 2^n)
+// Space: O(2^n)
+
+class Solution {
+public:
+    /**
+     * @param n the nth
+     * @return the nth sequence
+     */
+    string countAndSay(int n) {
+        string seq{"1"};
+        for (int i = 0; i < n - 1; ++i) {
+            seq = move(getNext(seq));
+        }
+        return seq;
+    }
+    
+    string getNext(const string& seq) {
+        stringstream  next_seq;
+        for (int i = 0; i < seq.length(); ++i) {
+            int cnt = 1;
+            while (i < seq.length() - 1 && seq[i] == seq[i + 1]) {
+                ++cnt, ++i;
+            }
+            next_seq << cnt << seq[i];
+        }
+        return next_seq.str();
+    }
+};
