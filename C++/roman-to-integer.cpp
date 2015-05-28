f1673043d7e4e4ef3a7e138c54cac3a43738afbd
kamyu <kamyu104@users.noreply.github.com>
2015-05-28 13:08:19 +0800

    Update roman-to-integer.cpp

--- C++/roman-to-integer.cpp
+++ C++/roman-to-integer.cpp
@@ -8,9 +8,9 @@ public:
      * @return an integer
      */
     int romanToInt(string& s) {
-        unordered_map<char, int> numeral_map =   {{'I',    1}, {'V',   5}, {'X',  10},
-                                                  {'L',   50}, {'C', 100}, {'D', 500},
-                                                  {'M', 1000}};
+        unordered_map<char, int> numeral_map = {{'I',    1}, {'V',   5}, {'X',  10},
+                                                {'L',   50}, {'C', 100}, {'D', 500},
+                                                {'M', 1000}};
         int decimal = 0;
         for (int i = 0; i < s.length(); ++i) {
             if (i > 0 && numeral_map[s[i]] > numeral_map[s[i - 1]]) {
