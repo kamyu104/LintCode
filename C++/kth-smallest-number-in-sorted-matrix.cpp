a20928306bae9e4e9ca003b5085c1a7656e076f2
kamyu <kamyu104@users.noreply.github.com>
2015-05-30 19:13:04 +0800

    Update kth-smallest-number-in-sorted-matrix.cpp

--- C++/kth-smallest-number-in-sorted-matrix.cpp
+++ C++/kth-smallest-number-in-sorted-matrix.cpp
@@ -1,6 +1,7 @@
-// Time:  O(k * log(min(n, m)))
-// Space: O(k)
+// Time:  O(k * log(min(m, n)))
+// Space: O(min(m, n))
 
+// BST solution.
 class Solution {
 public:
     /**
@@ -13,7 +14,7 @@ public:
         int kth_smallest = 0;
         
         // Find min number of height and width
-        if (true || matrix.size() < matrix[0].size()) {
+        if (matrix.size() < matrix[0].size()) {
             // Init BST by the first element of each row.
             for (int i = 0; i < matrix.size(); ++i) {
                 min_bst.emplace(move(pair<int, pair<int, int>>{matrix[i][0], {i, 0}}));
@@ -47,6 +48,69 @@ public:
                 // Insert the next possible element.
                 if (i + 1 < matrix.size()) {
                     min_bst.emplace(move(pair<int, pair<int, int>>{matrix[i + 1][j], {i + 1, j}}));
+                }
+            }
+        }
+        
+        return kth_smallest;
+    }
+};
+
+// Time:  O(k * log(min(m, n)))
+// Space: O(min(m, n))
+
+// Heap solution.
+class Solution {
+public:
+    struct Compare {
+        bool operator()(const pair<int, pair<int, int>>& a, const pair<int, pair<int, int>>& b) {
+            return a.first > b.first;
+        }
+    };
+    /**
+     * @param matrix: a matrix of integers
+     * @param k: an integer
+     * @return: the kth smallest number in the matrix
+     */
+    int kthSmallest(vector<vector<int> > &matrix, int k) {
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, Compare> min_heap;
+        int kth_smallest = 0;
+        
+        // Find min number of height and width
+        if (matrix.size() < matrix[0].size()) {
+            // Init Heap by the first element of each row.
+            for (int i = 0; i < matrix.size(); ++i) {
+                min_heap.emplace(move(pair<int, pair<int, int>>{matrix[i][0], {i, 0}}));
+            }
+            
+            while (k--) {
+                // Pop the min of Heap.
+                kth_smallest = min_heap.top().first;
+                int i = min_heap.top().second.first;
+                int j = min_heap.top().second.second;
+                min_heap.pop();
+                
+                // Insert the next possible element.
+                if (j + 1 < matrix[i].size()) {
+                    min_heap.emplace(move(pair<int, pair<int, int>>{matrix[i][j + 1], {i, j + 1}}));
+                }
+            }
+        } else {
+            // Init Heap by the first element of each column.
+            for (int j = 0; j < matrix[0].size(); ++j) {
+                min_heap.emplace(move(pair<int, pair<int, int>>{matrix[0][j], {0, j}}));
+            }
+            
+            while (k--) {
+                // Pop the min of Heap.
+                kth_smallest = min_heap.top().first;
+                int i = min_heap.top().second.first;
+                int j = min_heap.top().second.second;
+                min_heap.pop();
+                
+                // Insert the next possible element.
+                if (i + 1 < matrix.size()) {
+                    min_heap.emplace(move(pair<int, pair<int, int>>{matrix[i + 1][j], {i + 1, j}}));
                 }
             }
         }
