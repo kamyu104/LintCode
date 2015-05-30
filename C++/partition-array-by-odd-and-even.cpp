a363d8ce745319253ebe40a9234a7013fae69aae
kamyu <kamyu104@users.noreply.github.com>
2015-05-30 11:27:32 +0800

    Update partition-array-by-odd-and-even.cpp

--- C++/partition-array-by-odd-and-even.cpp
+++ C++/partition-array-by-odd-and-even.cpp
@@ -11,14 +11,14 @@ public:
         int left = 0;
         int right = nums.size() - 1;
         
-        while (left <= right) {
-            while (left <= right && nums[left] % 2) {
+        while (left < right) {
+            while (left <= right && nums[left] % 2 == 1) {
                 ++left;
             } 
             while (left <= right && nums[right] % 2 == 0) {
                 --right;
             }
-            if (left <= right) {
+            if (left < right) {
                swap(nums[left++], nums[right--]);
             }
         }
