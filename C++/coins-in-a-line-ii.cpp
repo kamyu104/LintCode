65978fad866de79cc5beed07850659f956774c4f
kamyu <kamyu104@users.noreply.github.com>
2015-05-31 07:51:30 +0800

    Update coins-in-a-line-ii.cpp

--- C++/coins-in-a-line-ii.cpp
+++ C++/coins-in-a-line-ii.cpp
@@ -8,18 +8,35 @@ public:
      * @param values: a vector of integers
      * @return: a boolean which equals to true if the first player will win
      */
+// Time:  O(n)
+// Space: O(1)
+
+// DP with rolling window.
+class Solution {
+public:
+    /**
+     * @param values: a vector of integers
+     * @return: a boolean which equals to true if the first player will win
+     */
     bool firstWillWin(vector<int> &values) {
+        // For corner case.
+        if (values.size() < 2) {
+            return values.size() % 2;
+        }
+        
         vector<int> P(5, 0);
         int sum = 0;
         // P[i] = max(values[i] + min(P[i + 2], P[i + 3]),
         //            values[i] + values[i + 1] + min(P[i + 3], P[i + 4]))
         for (int i = values.size() - 1; i >= 0; --i) {
             sum += values[i];
+            int values_i_plus_1 = i + 1 < values.size() ? values[i + 1]: 0;
             int a = i + 2 < values.size() ? P[(i + 2) % 5] : 0;
             int b = i + 3 < values.size() ? P[(i + 3) % 5] : 0;
             int c = i + 4 < values.size() ? P[(i + 4) % 5] : 0;
+            
             P[i % 5] = max(values[i] + min(a, b), 
-                       values[i] + values[i + 1] + min(b, c));
+                           values[i] + values_i_plus_1 + min(b, c));
         }
         
         return P[0] > sum - P[0];
@@ -42,11 +59,12 @@ public:
         //            values[i] + values[i + 1] + min(P[i + 3], P[i + 4]))
         for (int i = values.size() - 1; i >= 0; --i) {
             sum += values[i];
+            int values_i_plus_1 = i + 1 < values.size() ? values[i + 1]: 0;
             int a = i + 2 < values.size() ? P[i + 2] : 0;
             int b = i + 3 < values.size() ? P[i + 3] : 0;
             int c = i + 4 < values.size() ? P[i + 4] : 0;
             P[i] = max(values[i] + min(a, b), 
-                       values[i] + values[i + 1] + min(b, c));
+                       values[i] + values_i_plus_1 + min(b, c));
         }
         
         return P[0] > sum - P[0];
