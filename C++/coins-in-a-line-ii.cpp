5cc1c9c08ba00df8d8fa1d8f30d432744873dc12
kamyu <kamyu104@users.noreply.github.com>
2015-05-31 08:01:38 +0800

    Update coins-in-a-line-ii.cpp

--- C++/coins-in-a-line-ii.cpp
+++ C++/coins-in-a-line-ii.cpp
@@ -16,14 +16,14 @@ public:
         
         vector<int> P(5, 0);
         int sum = 0;
-        // P[i] = max(values[i] + min(P[i + 2], P[i + 3]),
-        //            values[i] + values[i + 1] + min(P[i + 3], P[i + 4]))
         for (int i = values.size() - 1; i >= 0; --i) {
             sum += values[i];
             int values_i_plus_1 = i + 1 < values.size() ? values[i + 1]: 0;
             int a = i + 2 < values.size() ? P[(i + 2) % 5] : 0;
             int b = i + 3 < values.size() ? P[(i + 3) % 5] : 0;
             int c = i + 4 < values.size() ? P[(i + 4) % 5] : 0;
+            // P[i] = max(values[i] + min(P[i + 2], P[i + 3]),
+            //            values[i] + values[i + 1] + min(P[i + 3], P[i + 4]))
             P[i % 5] = max(values[i] + min(a, b), 
                            values[i] + values_i_plus_1 + min(b, c));
         }
@@ -49,14 +49,14 @@ public:
         
         vector<int> P(values.size(), 0);
         int sum = 0;
-        // P[i] = max(values[i] + min(P[i + 2], P[i + 3]),
-        //            values[i] + values[i + 1] + min(P[i + 3], P[i + 4]))
         for (int i = values.size() - 1; i >= 0; --i) {
             sum += values[i];
             int values_i_plus_1 = i + 1 < values.size() ? values[i + 1]: 0;
             int a = i + 2 < values.size() ? P[i + 2] : 0;
             int b = i + 3 < values.size() ? P[i + 3] : 0;
             int c = i + 4 < values.size() ? P[i + 4] : 0;
+            // P[i] = max(values[i] + min(P[i + 2], P[i + 3]),
+            //            values[i] + values[i + 1] + min(P[i + 3], P[i + 4]))
             P[i] = max(values[i] + min(a, b), 
                        values[i] + values_i_plus_1 + min(b, c));
         }
