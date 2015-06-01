a7439392d5022a957cad2b5d028533c9a6704759
kamyu <kamyu104@users.noreply.github.com>
2015-06-02 01:07:29 +0800

    Create valid-parentheses.cpp

---
+++ C++/valid-parentheses.cpp
@@ -0,0 +1,29 @@
+// Time:  O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    /**
+     * @param s A string
+     * @return whether the string is a valid parentheses
+     */
+    bool isValidParentheses(string& s) {
+        const  unordered_map<char, char> symbol_pair = {{')', '('}, 
+                                                        {']', '['}, 
+                                                        {'}', '{'}};
+        stack<char> parentheses;
+        for (const auto& c: s) {
+            const auto& it = symbol_pair.find(c);
+            if (it != symbol_pair.cend()) {
+                if (parentheses.empty() || 
+                    parentheses.top() != it->second) {
+                    return false;
+                }
+                parentheses.pop();
+            } else {
+                parentheses.emplace(c);
+            }
+        }
+        return parentheses.empty();
+    }
+};
