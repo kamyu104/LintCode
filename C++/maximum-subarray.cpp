97b755b1ab60cbb4b240a81db0d93be22396c5f8
kamyu <kamyu104@users.noreply.github.com>
2015-06-02 23:51:44 +0800

    Update maximum-subarray.cpp

--- C++/maximum-subarray.cpp
+++ C++/maximum-subarray.cpp
@@ -9,12 +9,10 @@ public:
      */
     int maxSubArray(vector<int> nums) {
         int max_sum = INT_MIN, sum = 0;
-        for (auto& i : nums) {
+        for (const auto& i : nums) {
             sum += i;
             max_sum = max(max_sum, sum);
-            if (sum < 0) {
-                sum = 0;
-            }
+            sum = max(sum, 0);
         }
         return max_sum;
     }
