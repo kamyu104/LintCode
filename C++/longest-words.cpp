2ae57730869da1e5e6e0fb873527b522d81f0a7b
kamyu <kamyu104@users.noreply.github.com>
2015-06-04 10:18:18 +0800

    Create longest-words.cpp

---
+++ C++/longest-words.cpp
@@ -0,0 +1,23 @@
+// Time:  O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    /**
+     * @param dictionary: a vector of strings
+     * @return: a vector of strings
+     */
+    vector<string> longestWords(vector<string> &dictionary) {
+        vector<string> longest_words;
+        for (const auto& word : dictionary) {
+            if (!longest_words.empty() && 
+                word.length() > longest_words[0].length()) {
+                longest_words = move(vector<string>{word});
+            } else if (longest_words.empty() || 
+                       word.length() == longest_words[0].length()) {
+                longest_words.emplace_back(word);
+            }
+        }
+        return longest_words;
+    }
+};
