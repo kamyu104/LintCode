9bce84c7ac70bdae6d44be88bca5ad79963a93ad
kamyu <kamyu104@users.noreply.github.com>
2015-06-12 23:49:33 +0800

    Create invert-binary-tree.cpp

---
+++ C++/invert-binary-tree.cpp
@@ -0,0 +1,89 @@
+// Time:  O(n)
+// Space: O(h)
+
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+
+// Time:  O(n)
+// Space: O(w), w is the max number of nodes of the levels.
+// BFS solution.
+class Solution {
+public:
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
+    void invertBinaryTree(TreeNode *root) {
+        if (root != nullptr) {
+            queue<TreeNode*> nodes;
+            nodes.emplace(root);
+            while (!nodes.empty()) {
+                auto node = nodes.front();
+                nodes.pop();
+                swap(node->left, node->right);
+                if (node->left != nullptr) {
+                    nodes.emplace(node->left);
+                }
+                if (node->right != nullptr) {
+                    nodes.emplace(node->right);
+                }
+            }
+        }
+    }
+};
+
+// Time:  O(n)
+// Space: O(h)
+// Stack solution.
+class Solution2 {
+public:
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
+    void invertBinaryTree(TreeNode *root) {
+        if (root != nullptr) {
+            stack<TreeNode*> nodes;
+            nodes.emplace(root);
+            while (!nodes.empty()) {
+                auto node = nodes.top();
+                nodes.pop();
+                swap(node->left, node->right);
+                if (node->left != nullptr) {
+                    nodes.emplace(node->left);
+                }
+                if (node->right != nullptr) {
+                    nodes.emplace(node->right);
+                }
+            }
+        }
+    }
+};
+
+// Time:  O(n)
+// Space: O(h) 
+// DFS, Recursive solution.
+class Solution3 {
+public:
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
+    void invertBinaryTree(TreeNode *root) {
+        if (root != nullptr) {
+            invertBinaryTree(root->left);
+            invertBinaryTree(root->right);
+            swap(root->left, root->right);
+        }
+    }
+};
