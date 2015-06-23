140b3222172b27b7853514c5d791aa02b5814ab1
kamyu <kamyu104@users.noreply.github.com>
2015-06-23 21:09:38 +0800

    Update expression-evaluation.cpp

--- C++/expression-evaluation.cpp
+++ C++/expression-evaluation.cpp
@@ -47,13 +47,13 @@ public:
         const string op = operators.top();
         operators.pop();
         if (op == "+") {
-            operands.push(left + right);
+            operands.emplace(left + right);
         } else if (op == "-") {
-            operands.push(left - right);
+            operands.emplace(left - right);
         } else if (op == "*") {
-            operands.push(left * right);
+            operands.emplace(left * right);
         } else if (op == "/") {
-            operands.push(left / right);
+            operands.emplace(left / right);
         }
     }
 };
