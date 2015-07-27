510bfc3cc78141e2c60c47c6114c717a2a961e30
kamyu <kamyu104@users.noreply.github.com>
2015-07-27 08:49:09 +0800

    Update permutation-index-ii.cpp

--- C++/permutation-index-ii.cpp
+++ C++/permutation-index-ii.cpp
@@ -12,6 +12,36 @@ public:
         // Position 1 is paired with factor 0 and so is skipped.
         int position = 2;
         long long factor = 1;
+        map<int, int> number_to_count;
+        ++number_to_count[A.back()];
+        for (int i = static_cast<int>(A.size()) - 2; i >= 0; --i) {
+            ++number_to_count[A[i]];
+            for (const auto& kvp : number_to_count) {
+                if (kvp.first >= A[i]) {
+                    break;
+                } 
+                index += factor * kvp.second / number_to_count[A[i]];
+            }
+            factor = factor * position / number_to_count[A[i]];
+            ++position;
+        }
+        return index;
+    }
+};
+
+// Time:  O(N^2)
+// Space: O(N)
+class Solution2 {
+public:
+    /**
+     * @param A an integer array
+     * @return a long integer
+     */
+    long long permutationIndexII(vector<int>& A) {
+        long long index = 1;
+        // Position 1 is paired with factor 0 and so is skipped.
+        int position = 2;
+        long long factor = 1;
         unordered_map<int, int> number_to_count;
         ++number_to_count[A.back()];
         for (int i = static_cast<int>(A.size()) - 2; i >= 0; --i) {
@@ -34,7 +64,7 @@ public:
 
 // Time:  O(N^3)
 // Space: O(N)
-class Solution2 {
+class Solution3 {
 public:
     /**
      * @param A an integer array
