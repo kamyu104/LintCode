12669d3b98a6d7b28c7573ed0689808c61e57e8d
kamyu <kamyu104@users.noreply.github.com>
2015-08-07 01:44:05 +0800

    Update subarray-sum-closest.cpp

--- C++/subarray-sum-closest.cpp
+++ C++/subarray-sum-closest.cpp
@@ -19,8 +19,8 @@ public:
         vector<pair<int,int>> sum_from_start(n + 1, {0, 0});
         sum_from_start[0].second = -1; // For case closest sum is from 0.
         for (int  i = 0; i < n; ++i) {
-            sum_from_start[i+1].first = sum_from_start[i].first + nums[i];
-            sum_from_start[i+1].second = i;
+            sum_from_start[i + 1].first = sum_from_start[i].first + nums[i];
+            sum_from_start[i + 1].second = i;
         }
 
         // Sort each sum from start.
@@ -32,7 +32,7 @@ public:
 
         // Find min difference of adjacent sum.
         for (int i = 1; i <= n; ++i) {
-            int diff = abs(sum_from_start[i].first - sum_from_start[i-1].first);
+            int diff = abs(sum_from_start[i].first - sum_from_start[i - 1].first);
             if (diff < min_diff) {
                 min_diff = diff;
                 start = min(sum_from_start[i].second, sum_from_start[i - 1].second) + 1;
