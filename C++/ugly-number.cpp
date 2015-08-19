e892ff1d0b031bae78b9a754e46eae16a3809588
kamyu <kamyu104@users.noreply.github.com>
2015-08-19 09:52:24 +0800

    Update ugly-number.cpp

--- C++/ugly-number.cpp
+++ C++/ugly-number.cpp
@@ -1,11 +1,7 @@
 // Time:  O(k)
 // Space: O(1)
 
-#include <vector>
-#include <queue>
-
-using namespace std;
-
+// Heap solution.
 class Solution {
 public:
     /*
@@ -16,28 +12,53 @@ public:
         long long ugly_number = 0;
         priority_queue<long long, vector<long long>, greater<long long>> heap;
 
-        heap.emplace(3);
-        heap.emplace(5);
-        heap.emplace(7);
-
-        for (int i = 0; i < k; ++i) {
-            if (heap.top() % 3 == 0) {
-                ugly_number = heap.top();
-                heap.pop();
+        heap.emplace(1);
+        for (int i = 0; i <= k; ++i) {
+            ugly_number = heap.top();
+            heap.pop();
+            if (ugly_number % 3 == 0) {
                 heap.emplace(ugly_number * 3);
             }
-            else if (heap.top() % 5 == 0) {
-                ugly_number = heap.top();
-                heap.pop();
+            else if (ugly_number % 5 == 0) {
                 heap.emplace(ugly_number * 3);
                 heap.emplace(ugly_number * 5);
             }
             else {
-                ugly_number = heap.top();
-                heap.pop();
                 heap.emplace(ugly_number * 3);
                 heap.emplace(ugly_number * 5);
                 heap.emplace(ugly_number * 7);
+            }
+        }
+        return ugly_number;
+    }
+};
+
+// BST solution.
+class Solution2 {
+public:
+    /*
+     * @param k: The number k.
+     * @return: The kth prime number as description.
+     */
+    long long kthPrimeNumber(int k) {
+        long long ugly_number = 0;
+        set<long long> bst;
+
+        bst.emplace(1);
+        for (int i = 0; i <= k; ++i) {
+            ugly_number = *bst.cbegin();
+            bst.erase(bst.cbegin());
+            if (ugly_number % 3 == 0) {
+                bst.emplace(ugly_number * 3);
+            }
+            else if (ugly_number % 5 == 0) {
+                bst.emplace(ugly_number * 3);
+                bst.emplace(ugly_number * 5);
+            }
+            else {
+                bst.emplace(ugly_number * 3);
+                bst.emplace(ugly_number * 5);
+                bst.emplace(ugly_number * 7);
             }
         }
         return ugly_number;
