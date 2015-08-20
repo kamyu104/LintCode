a3f3255bf3d8cb4f35d351580eb867506a4874cb
kamyu <kamyu104@users.noreply.github.com>
2015-08-21 01:09:30 +0800

    Update single-number.cpp

--- C++/single-number.cpp
+++ C++/single-number.cpp
@@ -3,6 +3,17 @@
 
 class Solution {
 public:
+	/**
+	 * @param A: Array of integers.
+	 * return: The single number.
+	 */
+    int singleNumber(vector<int> &A) {
+        return accumulate(A.cbegin(), A.cend(), 0, std::bit_xor<int>());
+    }
+};
+
+class Solution2 {
+public:
     /**
      * @param A: Array of integers.
      * return: The single number.
