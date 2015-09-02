371c4f595eebcfa0d15bba763d5887baa93d8ade
kamyu <kamyu104@users.noreply.github.com>
2015-09-03 00:20:41 +0800

    Update single-number-iii.cpp

--- C++/single-number-iii.cpp
+++ C++/single-number-iii.cpp
@@ -9,6 +9,30 @@ public:
      */
     vector<int> singleNumberIII(vector<int> &A) {
         // Xor all the elements to get x ^ y.
+        int x_xor_y = accumulate(A.cbegin(), A.cend(), 0, bit_xor<int>());
+
+        // Get the last bit where 1 occurs.
+        x_xor_y &= -x_xor_y;
+
+        // Get the subset of A where the number has the bit.
+        // The subset only contains one of the two integers, call it x.
+        // Xor all the elemets in the subset to get x.
+        vector<int> result(2, 0);
+        for (const auto& i : A) {
+            result[!(i & x_xor_y)] ^= i;
+        }
+        return result;
+    }
+};
+
+class Solution2 {
+public:
+    /**
+     * @param A : An integer array
+     * @return : Two integers
+     */
+    vector<int> singleNumberIII(vector<int> &A) {
+        // Xor all the elements to get x ^ y.
         int x_xor_y = 0;
         for (const auto& i : A) {
             x_xor_y ^= i;
