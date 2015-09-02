fd2d4d8a174ee5d86077082ea120a4397c3ae11f
kamyu <kamyu104@users.noreply.github.com>
2015-09-03 00:48:02 +0800

    Update single-number-iii.cpp

--- C++/single-number-iii.cpp
+++ C++/single-number-iii.cpp
@@ -12,14 +12,14 @@ public:
         int x_xor_y = accumulate(A.cbegin(), A.cend(), 0, bit_xor<int>());
 
         // Get the last bit where 1 occurs.
-        x_xor_y &= -x_xor_y;
+        const auto bit = x_xor_y & -x_xor_y;
 
         // Get the subset of A where the number has the bit.
         // The subset only contains one of the two integers, call it x.
         // Xor all the elemets in the subset to get x.
         vector<int> result(2, 0);
         for (const auto& i : A) {
-            result[static_cast<bool>(i & x_xor_y)] ^= i;
+            result[static_cast<bool>(i & bit)] ^= i;
         }
         return result;
     }
