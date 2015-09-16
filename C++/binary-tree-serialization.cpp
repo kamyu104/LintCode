b38886ba483ed8e7cfefd57479286a28c4b5e448
kamyu <kamyu104@users.noreply.github.com>
2015-09-17 00:41:13 +0800

    Update binary-tree-serialization.cpp

--- C++/binary-tree-serialization.cpp
+++ C++/binary-tree-serialization.cpp
@@ -14,6 +14,32 @@
  * }
  */
 class Solution {
+public:
+    /**
+     * This method will be invoked first, you should design your own algorithm
+     * to serialize a binary tree which denote by a root node to a string which
+     * can be easily deserialized by your own "deserialize" method later.
+     */
+    string serialize(TreeNode *root) {
+        string output;
+        serializeHelper(root, &output);
+        return output;
+    }
+
+    /**
+     * This method will be invoked second, the argument data is what exactly
+     * you serialized at method "serialize", that means the data is not given by
+     * system, it's given by your own serialize method. So the format of data is
+     * designed by yourself, and deserialize it here as you serialize it in
+     * "serialize" method.
+     */
+    TreeNode *deserialize(string data) {
+        TreeNode *root = nullptr;
+        int start = 0;
+        deserializeHelper(data, &start, &root);
+        return root;
+    }
+
 private:
     bool getNumber(const string &data, int *start, int *num) {
         if (data[*start] == '#') {
@@ -49,31 +75,5 @@ private:
             serializeHelper(root->left, prev);
             serializeHelper(root->right, prev);
         }
-    }
-
-public:
-    /**
-     * This method will be invoked first, you should design your own algorithm
-     * to serialize a binary tree which denote by a root node to a string which
-     * can be easily deserialized by your own "deserialize" method later.
-     */
-    string serialize(TreeNode *root) {
-        string output;
-        serializeHelper(root, &output);
-        return output;
-    }
-
-    /**
-     * This method will be invoked second, the argument data is what exactly
-     * you serialized at method "serialize", that means the data is not given by
-     * system, it's given by your own serialize method. So the format of data is
-     * designed by yourself, and deserialize it here as you serialize it in
-     * "serialize" method.
-     */
-    TreeNode *deserialize(string data) {
-        TreeNode *root = nullptr;
-        int start = 0;
-        deserializeHelper(data, &start, &root);
-        return root;
     }
 };
