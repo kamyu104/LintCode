a38a8028f4b82babf5f8e7510e3b4618a48f7f4d
kamyu <kamyu104@users.noreply.github.com>
2015-09-27 17:40:56 +0800

    Create cosine-similarity.py

---
+++ Python/cosine-similarity.py
@@ -0,0 +1,22 @@
+# Time:  O(n)
+# Space: O(1)
+
+from math import sqrt
+
+class Solution:
+    """
+    @param A: An integer array.
+    @param B: An integer array.
+    @return: Cosine similarity.
+    """
+    def cosineSimilarity(self, A, B):
+        invalid = 2
+        if len(A) != len(B):
+            return invalid
+        A_dot_B, A_dot_A, B_dot_B = 0.0, 0.0, 0.0
+        for i in xrange(len(A)):
+            A_dot_B += A[i] * B[i]
+            A_dot_A += A[i] * A[i]
+            B_dot_B += B[i] * B[i]
+        return A_dot_B / sqrt(A_dot_A) / sqrt(B_dot_B) if A_dot_A and B_dot_B \
+                                                       else invalid
