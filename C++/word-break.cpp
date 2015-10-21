68c656a85e94c1acf5f9a6235c798010a40fa336
kamyu <kamyu104@users.noreply.github.com>
2015-10-21 21:26:52 +0800

    Update word-break.cpp

--- C++/word-break.cpp
+++ C++/word-break.cpp
@@ -7,17 +7,18 @@ public:
      * @param s: A string s
      * @param dict: A dictionary of words dict
      */
-    bool wordSegmentation(string s, unordered_set<string> &dict) {
+    bool wordBreak(string s, unordered_set<string> &dict) {
         const int n = s.length();
         if (n < 1) {
             return true;
         }
 
-        // Filter out impossible string which alphabet set is not covered by dict.
+        // Filter out impossible string which alphabet set is not covered
+        // by dict.
         unordered_set<char> chrs;
         for (const auto& word : dict) {
             for (const auto& c : word) {
-                chrs.insert(c);
+                chrs.emplace(c);
             }
         } 
         for (const auto& c : s) {
@@ -29,7 +30,8 @@ public:
         vector<bool> canBreak(n, false);
         for (int i = 0; i < n; ++i) {
             for (int j = i; j >= 0; --j) {
-                if ((j == 0 || canBreak[j-1]) && dict.count(s.substr(j, i - j + 1))) {
+                if ((j == 0 || canBreak[j - 1]) && 
+                    dict.count(s.substr(j, i - j + 1))) {
                     canBreak[i] = true;
                     break;
                 }
