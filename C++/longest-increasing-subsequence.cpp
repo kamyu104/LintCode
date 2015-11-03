44d93f7c2f06256c158ad48c3dc2869432031f42
kamyu <kamyu104@users.noreply.github.com>
2015-11-04 01:22:44 +0800

    Update longest-increasing-subsequence.cpp

--- C++/longest-increasing-subsequence.cpp
+++ C++/longest-increasing-subsequence.cpp
@@ -52,9 +52,9 @@ public:
 private:
     void insert(vector<int> *LIS, const int target) {
         int left = 0, right = LIS->size() - 1;
-        auto comp = [](int x, int target) { return x >= target; };
+        auto comp = [](int x, int target) { return x > target; };
 
-        // Find the first index "left" which satisfies LIS[left] >= target
+        // Find the first index "left" which satisfies LIS[left] > target
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (comp((*LIS)[mid], target)) {
