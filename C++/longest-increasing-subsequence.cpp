58df255c73b9e6bc140a447c5d2fc7d17ad9566a
kamyu <kamyu104@users.noreply.github.com>
2015-11-03 23:24:27 +0800

    Update longest-increasing-subsequence.cpp

--- C++/longest-increasing-subsequence.cpp
+++ C++/longest-increasing-subsequence.cpp
@@ -7,30 +7,6 @@ public:
      * @param nums: The integer array
      * @return: The length of LIS (longest increasing subsequence)
      */
-    void insert(vector<int>& LIS, int target) {
-        int left = 0, right = LIS.size() - 1;
-        auto comp = [](int x, int target) { return x <= target; };
-
-        // Find the largest index "left" which satisfies LIS[left] <= target
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (!comp(LIS[mid], target)) {
-                right = mid - 1;
-            }
-            else {
-                left = mid + 1;
-            }
-        }
-
-        // If not found, append the target.
-        if (left == LIS.size()) {
-            LIS.emplace_back(target);
-        }
-        else {
-            LIS[left] = target;
-        }
-    }
-
     int longestIncreasingSubsequence(vector<int> nums) {
         vector<int> LIS;
 
@@ -39,6 +15,29 @@ public:
         }
 
         return LIS.size();
+    }
+
+private:
+    void insert(vector<int>& LIS, int target) {
+        int left = 0, right = LIS.size() - 1;
+        auto comp = [](int x, int target) { return x > target; };
+
+        // Find the largest index "left" which satisfies LIS[left] > target
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (comp(LIS[mid], target)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        // If not found, append the target.
+        if (left == LIS.size()) {
+            LIS.emplace_back(target);
+        } else {
+            LIS[left] = target;
+        }
     }
 };
 
