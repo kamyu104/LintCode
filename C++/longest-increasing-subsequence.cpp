9b2c2497e9d8321c6a6a4a7956d06d9074a9280d
kamyu <kamyu104@users.noreply.github.com>
2015-11-04 01:20:50 +0800

    Update longest-increasing-subsequence.cpp

--- C++/longest-increasing-subsequence.cpp
+++ C++/longest-increasing-subsequence.cpp
@@ -1,6 +1,7 @@
 // Time:  O(nlogn)
 // Space: O(n)
 
+// Binary search solution with STL.
 class Solution {
 public:
     /**
@@ -11,14 +12,45 @@ public:
         vector<int> LIS;
 
         for (const auto& i : nums) {
-            insert(LIS, i);
+            insert(&LIS, i);
         }
 
         return LIS.size();
     }
 
 private:
-    void insert(vector<int>& LIS, int target) {
+    void insert(vector<int> *LIS, const int target) {
+        // Find the first index "left" which satisfies LIS[left] > target
+        auto it = upper_bound(LIS->begin(), LIS->end(), target);
+
+        // If not found, append the target.
+        if (it != LIS->end()) {
+            *it = target;
+        } else {
+            LIS->emplace_back(target);
+        }
+    }
+};
+
+// Binary search solution.
+class Solution2 {
+public:
+    /**
+     * @param nums: The integer array
+     * @return: The length of LIS (longest increasing subsequence)
+     */
+    int longestIncreasingSubsequence(vector<int> nums) {
+        vector<int> LIS;
+
+        for (const auto& i : nums) {
+            insert(&LIS, i);
+        }
+
+        return LIS.size();
+    }
+
+private:
+    void insert(vector<int> *LIS, const int target) {
         int left = 0, right = LIS.size() - 1;
         auto comp = [](int x, int target) { return x > target; };
 
