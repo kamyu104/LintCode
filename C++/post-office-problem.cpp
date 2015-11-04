c971374c9f55c80093dd3d97b72f35faf0ae05e6
kamyu <kamyu104@users.noreply.github.com>
2015-11-04 12:14:12 +0800

    Update post-office-problem.cpp

--- C++/post-office-problem.cpp
+++ C++/post-office-problem.cpp
@@ -1,5 +1,5 @@
 // Time:  O(k * n^2)
-// Space: O(n^2)
+// Space: O(n)
 
 class Solution {
 public:
@@ -18,9 +18,9 @@ public:
 
         // Precompute cost.
         // Time:  O(n^2)
-        // Space: O(n^2)
-        vector<vector<int>> cost(A.size() + 1, vector<int>(A.size() + 1, 0));
-        computeMinCost(A, &cost);
+        // Space: O(n)
+        vector<int> before_cost(n), after_cost(n);
+        computeCost(A, &before_cost, &after_cost);
 
         // DP of sum.
         // Time:  O(k * n^2)
@@ -37,7 +37,9 @@ public:
                     if (sum[(i - 1) % 2][j - r] != INT_MAX) {
                         sum[i % 2][j] = min(sum[i % 2][j],
                                             sum[(i - 1) % 2][j - r] +
-                                            cost[j - r + 1][j]);
+                                            minCost(A,
+                                                    before_cost, after_cost,
+                                                    j - r, j - 1));
                     }
                 }
             }
@@ -45,30 +47,33 @@ public:
         return sum[k % 2][n];
     }
 
-    void computeMinCost(const vector<int>& A, vector<vector<int>> *cost) {
-        // Min cost of building a post office between house (i, j).
-        // This post office must be in median position.
+    void computeCost(const vector<int>& A,
+                     vector<int> *before_cost, vector<int> *after_cost) {
         const int n = A.size();
-        vector<int> before_diff(n), after_diff(n);
         for (int i = 1; i < n; ++i) {
-            // before_diff[i] = Sum(A[i] - A[k]) for k < i
-            before_diff[i] = before_diff[i - 1] + i * (A[i] - A[i - 1]);
+            // before_cost[i] = Sum(A[i] - A[k]) for k < i
+            (*before_cost)[i] = (*before_cost)[i - 1] +
+                                i * (A[i] - A[i - 1]);
         }
         for (int i = n - 2; i >= 0; --i) {
-            // after_diff[i] = Sum(A[k] - A[i]) for k > i
-            after_diff[i] = after_diff[i + 1] + (n - 1 - i) * (A[i + 1] - A[i]);
+            // after_cost[i] = Sum(A[k] - A[i]) for k > i
+            (*after_cost)[i] = (*after_cost)[i + 1] +
+                            (n - 1 - i) * (A[i + 1] - A[i]);
         }
-        
-        for (int i = 0; i < n; ++i) {
-            for (int j = i; j < n; ++j) {
-                int mid = i + (j - i) / 2;
-                int before_mid = before_diff[mid] -
-                                 (before_diff[i] + i * (A[mid] - A[i]));
-                int after_mid = after_diff[mid] -
-                                (after_diff[j] + (n - 1 - j) * (A[j] - A[mid]));
-                (*cost)[i + 1][j + 1] +=  before_mid + after_mid;
-            }
-        }
+    }
+    
+    int minCost(const vector<int>& A,
+                const vector<int> &before_cost, const vector<int> &after_cost,
+                const int i, const int j) {
+            // Min cost of building a post office between house (i, j).
+            // This post office must be in median position.
+            const int n = A.size();
+            int mid = i + (j - i) / 2;
+            int before_mid = before_cost[mid] -
+                             (before_cost[i] + i * (A[mid] - A[i]));
+            int after_mid = after_cost[mid] -
+                            (after_cost[j] + (n - 1 - j) * (A[j] - A[mid]));
+            return before_mid + after_mid;
     }
 };
 
