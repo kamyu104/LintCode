30289299e59f6a5b38060877d05dc21321496a27
kamyu <kamyu104@users.noreply.github.com>
2015-12-03 23:09:43 +0800

    Create graph-valid-tree.cpp

---
+++ C++/graph-valid-tree.cpp
@@ -0,0 +1,49 @@
+// Time:  O(|V| + |E|)
+// Space: O(|V| + |E|)
+
+class Solution {
+public:
+    struct node {
+        int parent = -1;
+        vector<int>neighbors;
+    };
+
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if (edges.size() != n - 1) {
+            return false;
+        } else if (n == 1) {
+            return true;
+        }
+
+        unordered_map<int, node> nodes;
+        for (const auto& edge : edges) {
+            nodes[edge.front()].neighbors.emplace_back(edge.back());
+            nodes[edge.back()].neighbors.emplace_back(edge.front());
+        }
+
+        if (nodes.size() != n) {
+            return false;
+        }
+
+        unordered_set<int> visited;
+        queue<int> q;
+        q.emplace(0);
+        while (!q.empty()) {
+            const int i = q.front();
+            q.pop();
+            visited.emplace(i);
+            for (const auto& node : nodes[i].neighbors) {
+                if (node != nodes[i].parent) {
+                    if (visited.find(node) != visited.end()) {
+                        return false;
+                    } else {
+                        visited.emplace(node);
+                        nodes[node].parent = i;
+                        q.emplace(node);
+                    }
+                }
+            }
+        }
+        return visited.size() == n;
+    }
+};
