5087e60d88afedfba2c677951202545571b885c9
kamyu <kamyu104@users.noreply.github.com>
2015-12-21 22:08:26 +0800

    Create restore-ip-addresses.cpp

---
+++ C++/restore-ip-addresses.cpp
@@ -0,0 +1,48 @@
+// Time:  O(n^m) = O(3^4)
+// Space: O(n * m) = O(3 * 4)
+
+class Solution {
+public:
+    /**
+     * @param s the IP string
+     * @return All possible valid IP addresses
+     */
+    vector<string> restoreIpAddresses(string& s) {
+        vector<string> result;
+        string cur;
+        restoreIpAddressesHelper(s, 0, 0, &cur, &result);
+        return result;
+    }
+        
+    void restoreIpAddressesHelper(const string& s,
+                                  int start, int dots,
+                                  string *cur,
+                                  vector<string> *result) {
+        // pruning to improve performance
+        if (((4 - dots) * 3 < s.length() - start) ||
+            ((4 - dots) > s.length() - start)) {
+            return;
+        }
+        
+        if (start == s.length() && dots == 4) {
+            result->emplace_back(cur->begin(), prev(cur->end()));
+        } else {
+            for (int i = start; i < start + 3; ++i) {
+                string tmp = s.substr(start, i - start + 1);
+                if (i < s.length() && isValid(tmp)) {
+                    tmp.push_back('.');
+                    cur->append(tmp);
+                    restoreIpAddressesHelper(s, i + 1, dots + 1, cur, result);
+                    cur->resize(cur->length() - (i - start + 2));
+                }
+            }
+        }
+    }
+        
+    bool isValid(const string &s) {
+        if (s.empty() || (s[0] == '0' && s != "0")) {
+            return false;
+        }
+        return stoi(s) < 256;
+    }
+};
