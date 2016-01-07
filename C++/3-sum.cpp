ff5a9496244c1dc375ce9beca757f9425a6bbee5
kamyu <kamyu104@users.noreply.github.com>
2016-01-07 22:17:35 +0800

    Update 3-sum.cpp

--- C++/3-sum.cpp
+++ C++/3-sum.cpp
@@ -14,30 +14,24 @@ public:
         // Make nums in increasing order. Time: O(nlogn)
         sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < nums.size() - 2; ++i) {
-            // Skip duplicated.
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            int j = i + 1;
-            int k = nums.size() - 1;
-
-            while (j < k) {  // Time: O(n) for each i.
-                if (j - 1 > i  && nums[j] == nums[j - 1]) {  // Skip duplicated.
-                    ++j;
-                } else if (k + 1 < nums.size() && nums[k] == nums[k + 1]) {  // Skip duplicated.
-                    --k;
-                } else {
-                    const auto sum = nums[i] + nums[j] + nums[k];
-
-                    if (sum > target) {  // Should decrease sum.
-                        --k;
-                    } else if (sum < target) {  // Should increase sum.
+        for (int i = 0; i < static_cast<int>(nums.size()) - 2; ++i) {
+            if (i == 0 || nums[i] != nums[i - 1]) {  // Skip duplicated.
+                for (int j = i + 1, k = nums.size() - 1; j < k; ) {  // Time: O(n) for each i.
+                    if (j - 1 > i  && nums[j] == nums[j - 1]) {  // Skip duplicated.
                         ++j;
+                    } else if (k + 1 < nums.size() && nums[k] == nums[k + 1]) {  // Skip duplicated.
+                        --k;
                     } else {
-                        ans.emplace_back(vector<int>{nums[i], nums[j], nums[k]});
-                        ++j;
-                        --k;
+                        const auto sum = nums[i] + nums[j] + nums[k];
+                        if (sum > target) {  // Should decrease sum.
+                            --k;
+                        } else if (sum < target) {  // Should increase sum.
+                            ++j;
+                        } else {
+                            ans.emplace_back(vector<int>{nums[i], nums[j], nums[k]});
+                            ++j;
+                            --k;
+                        }
                     }
                 }
             }
