ff463bb60c01001592076db9c67bb265780111e9
kamyu <kamyu104@users.noreply.github.com>
2016-02-22 22:18:07 +0800

    Update reverse-words-in-a-string.cpp

--- C++/reverse-words-in-a-string.cpp
+++ C++/reverse-words-in-a-string.cpp
@@ -11,20 +11,20 @@ public:
         // Reverse the whole string first.
         reverse(s.begin(), s.end());
 
-        size_t start = 0, end;
-        while ((end = s.find(" ", start)) != string::npos) {
+        size_t begin = 0, end = 0, len = 0;
+        while ((begin = s.find_first_not_of(" ", end)) != string::npos) {
+            if ((end = s.find(" ", begin)) == string::npos) {
+                end = s.length();
+            }
             // Reverse each word in the string.
-            reverse(s.begin() + start, s.begin() + end);
-            start = end + 1;
-        }
-        // Reverse the last word.
-        reverse(s.begin() + start, s.end());
+            reverse(s.begin() + begin, s.begin() + end);
 
-        // Remove beginning blank.
-        if ((start = s.find_first_not_of(" ")) != string::npos) {
-            return s.substr(start);
+            // Shift the word to avoid extra space.
+            move(s.begin() + begin, s.begin() + end, s.begin() + len);
+            len += end - begin;
+            s[len++] = ' ';
         }
-
+        s.resize(len ? len - 1 : 0);
         return s;
     }
 };
