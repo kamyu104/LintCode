148a51cce504778b1b05bcbcdd098211388cd415
kamyu <kamyu104@users.noreply.github.com>
2016-03-04 22:14:55 +0800

    Create wiggle-sort.cpp

---
+++ C++/wiggle-sort.cpp
@@ -0,0 +1,19 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    /**
+     * @param nums a list of integer
+     * @return void
+     */  
+    void wiggleSort(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); ++i) {
+            if (((i % 2) && nums[i] < nums[i - 1]) ||
+                (!(i % 2) && nums[i] > nums[i - 1])) {
+                // Swap unordered elements.
+                swap(nums[i], nums[i - 1]);
+            }
+        }
+    }
+};
