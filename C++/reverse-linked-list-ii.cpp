d8a75eb6ba6b6796fc0f0ef9dd3cd229f9863173
kamyu <kamyu104@users.noreply.github.com>
2016-03-15 19:42:23 +0800

    Update reverse-linked-list-ii.cpp

--- C++/reverse-linked-list-ii.cpp
+++ C++/reverse-linked-list-ii.cpp
@@ -2,46 +2,35 @@
 // Space: O(1)
 
 /**
- * Definition of singly-linked-list:
- *
- * class ListNode {
- * public:
+ * Definition for singly-linked list.
+ * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int val) {
- *        this->val = val;
- *        this->next = NULL;
- *     }
- * }
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
  */
 class Solution {
 public:
-    /**
-     * @param head: The head of linked list.
-     * @param m: The start position need to reverse.
-     * @param n: The end position need to reverse.
-     * @return: The new head of partial reversed linked list.
-     */
-    ListNode *reverseBetween(ListNode *head, int m, int n) {
-        ListNode dummy(INT_MIN);
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        ListNode dummy{0};
         dummy.next = head;
 
-        ListNode *prev = &dummy;
+        auto *prev = &dummy;
 
         for (int i = 0; i < m - 1; ++i) {
             prev = prev->next;
         }
 
-        ListNode *head2 = prev;
+        auto *head2 = prev;
 
         prev = prev->next;
-        ListNode *cur = prev->next;
+        auto *cur = prev->next;
 
         for (int i = m; i < n; ++i) {
-            prev->next = cur->next;  // remove cur from the list
-            cur->next = head2->next; // add cur to the head
-            head2->next = cur;       // add cur to the head
-            cur = prev->next;        // get next cur
+            prev->next = cur->next;  // Remove cur from the list.
+            cur->next = head2->next; // Add cur to the head.
+            head2->next = cur;       // Add cur to the head.
+            cur = prev->next;        // Get next cur.
         }
 
         return dummy.next;
