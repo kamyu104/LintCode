d1350c8f8c83342d72cdf7e4b88a876fc1d670dc
kamyu <kamyu104@users.noreply.github.com>
2016-04-16 15:37:24 +0800

    Create left-pad.cpp

---
+++ C++/left-pad.cpp
@@ -0,0 +1,18 @@
+// Time:  O(p + n), p is the number of padChars
+// Space: O(1)
+
+class StringUtils {
+public:
+    /**
+     * @param originalStr the string we want to append to
+     * @param size the target length of the string
+     * @param padChar the character to pad to the left side of the string
+     * @return a string
+     */
+    static string leftPad(string& originalStr, int size, char padChar=' ') {
+
+        return string(max(0,
+                          size - static_cast<int>(originalStr.length())),
+                      padChar).append(originalStr);
+    }
+};
