cba0627f76157bc6a127bfd590ef8c4ba3a4f089
kamyu <kamyu104@users.noreply.github.com>
2016-05-04 00:28:46 +0800

    Update subsets.cpp

--- C++/subsets.cpp
+++ C++/subsets.cpp
@@ -28,3 +28,24 @@ public:
         return ans;
     }
 };
+
+// Time:  O(n * 2^n)
+// Space: O(1)
+class Solution {
+public:
+    /**
+     * @param S: A set of numbers.
+     * @return: A list of lists. All valid subsets.
+     */
+    vector<vector<int> > subsets(vector<int> &nums) {
+        vector<vector<int>> result(1);
+        for (size_t i = 0; i < nums.size(); ++i) {
+            const size_t size = result.size();
+            for (size_t j = 0; j < size; ++j) {
+                result.emplace_back(result[j]);
+                result.back().emplace_back(nums[i]);
+            }
+        }
+        return result;
+    }
+};
