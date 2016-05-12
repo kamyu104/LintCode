476160e102e8a4f19f5330a331fbd81c815cb23f
kamyu <kamyu104@users.noreply.github.com>
2016-05-12 22:45:27 +0800

    Update search-in-rotated-sorted-array.cpp

--- C++/search-in-rotated-sorted-array.cpp
+++ C++/search-in-rotated-sorted-array.cpp
@@ -2,33 +2,24 @@
 // Space: O(1)
 
 class Solution {
-    /**
+    /** 
      * param A : an integer ratated sorted array
      * param target :  an integer to be searched
      * return : an integer
      */
 public:
     int search(vector<int> &A, int target) {
-        int left = 0;
-        int right = A.size() - 1;
+        int left = 0, right = A.size() - 1;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (A[mid] == target) {
                 return mid;
-            }
-            else if (A[left] <= A[mid]) {
-                if (A[left] <= target && target < A[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+            } else if ((A[mid] >= A[left] && A[left] <= target && target < A[mid]) ||
+                        (A[mid] < A[left] && !(A[mid] < target && target <= A[right]))) {
+                right = mid - 1;
             } else {
-                if (A[mid] < target && target <= A[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
+                left = mid + 1;
             }
         }
 
@@ -37,33 +28,24 @@ public:
 };
 
 class Solution2 {
-    /**
+    /** 
      * param A : an integer ratated sorted array
      * param target :  an integer to be searched
      * return : an integer
      */
 public:
     int search(vector<int> &A, int target) {
-        int left = 0;
-        int right = A.size();
+        int left = 0, right = A.size();
 
         while (left < right) {
             int mid = left + (right - left) / 2;
             if (A[mid] == target) {
                 return mid;
-            }
-            else if (A[left] <= A[mid]) {
-                if (A[left] <= target && target < A[mid]) {
-                    right = mid;
-                } else {
-                    left = mid + 1;
-                }
+            } else if ((A[left] <= A[mid] && A[left] <= target && target < A[mid]) ||
+                       (A[left] > A[mid] && !(A[mid] < target && target <= A[right - 1]))) {
+                right = mid;
             } else {
-                if (A[mid] < target && target <= A[right - 1]) {
-                    left = mid + 1;
-                } else {
-                    right = mid;
-                }
+                left = mid + 1;
             }
         }
 
