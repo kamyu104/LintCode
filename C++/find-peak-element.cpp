68afe6317abedb15fd6c81fd607e9b1e746e708c
kamyu <kamyu104@users.noreply.github.com>
2016-05-12 22:16:54 +0800

    Update find-peak-element.cpp

--- C++/find-peak-element.cpp
+++ C++/find-peak-element.cpp
@@ -8,53 +8,20 @@ public:
      * @return: return any of peek positions.
      */
     int findPeak(vector<int> A) {
-        int left = 1;
-        int right = A.size() - 1;
-
+        int left = 0, right = A.size() - 1;
+        
         while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (A[mid - 1] <= A[mid] && A[mid] >= A[mid + 1]) {
+            const auto mid = left + (right - left) / 2;
+            if ((mid == 0 || A[mid - 1] < A[mid]) &&
+               (mid + 1 == A.size() || A[mid] > A[mid + 1])) {
                 return mid;
-            } else if (A[mid - 1] > A[mid]) {
+            } else if (!(mid == 0 || A[mid - 1] < A[mid])) {
                 right = mid;
             } else {
                 left = mid + 1;
             }
         }
-
+       
         return left;
     }
 };
-
-class Solution2 {
-public:
-    /**
-     * @param A: An integers array.
-     * @return: return any of peek positions.
-     */
-    int findPeak(vector<int> A) {
-        int l = 0;
-        int r = A.size() - 1;
-
-        while (l < r) {
-            int mid = l + (r - l) / 2;
-
-            // If mid is a peak
-            if ((mid == 0 || A[mid - 1] <= A[mid]) &&
-                (mid == A.size() - 1 || A[mid] >= A[mid + 1])) {
-                return mid;
-            }
-            // If mid - 1 is a possible peak
-            else if (mid > 0 && A[mid - 1] > A[mid]) {
-                r = mid - 1;
-            }
-            // Else: A[mid - 1] <= A[mid] < A[mid + 1]
-            else {
-                l = mid + 1;
-            }
-        }
-
-        return l;
-    }
-};
-
