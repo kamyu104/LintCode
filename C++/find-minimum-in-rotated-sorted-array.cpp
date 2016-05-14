a9faea81e8ef854f5d76e068820032f6b8b7f05c
kamyu <kamyu104@users.noreply.github.com>
2016-05-14 11:47:37 +0800

    Update find-minimum-in-rotated-sorted-array.cpp

--- C++/find-minimum-in-rotated-sorted-array.cpp
+++ C++/find-minimum-in-rotated-sorted-array.cpp
@@ -8,12 +8,34 @@ public:
      * @return: the minimum number in the array
      */
     int findMin(vector<int> &num) {
-        int left = 0;
-        int right = num.size() - 1;
+        int left = 0, right = num.size() - 1;
+        const auto target = num.back();
+
+        while (left <= right) {
+            const auto  mid = left + (right - left) / 2;
+            if (num[mid] <= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return num[left];
+    }
+};
+
+class Solution2 {
+public:
+    /**
+     * @param num: a rotated sorted array
+     * @return: the minimum number in the array
+     */
+    int findMin(vector<int> &num) {
+        int left = 0, right = num.size() - 1;
 
         // Find min left s.t. num[left] < num[left'].
         while (left < right && num[left] >= num[right]) {
-            int mid = left + (right - left) / 2;
+            const auto mid = left + (right - left) / 2;
             if (num[mid] < num[left]) {
                 right = mid;
             } else {
