f81cce34d57d33c2187a3a35c0572dc13fc5ad71
kamyu <kamyu104@users.noreply.github.com>
2016-05-15 12:41:38 +0800

    Update binary-tree-zigzag-level-order-traversal.cpp

--- C++/binary-tree-zigzag-level-order-traversal.cpp
+++ C++/binary-tree-zigzag-level-order-traversal.cpp
@@ -14,7 +14,6 @@
  * }
  */
 
-
 // Time:  O(n)
 // Space: O(n)
 
@@ -31,7 +30,58 @@
  * }
  */
 
+// Using stack.
 class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: A list of lists of integer include
+     *          the zigzag level order traversal of its nodes' values
+     */
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode *root) {
+        vector<vector<int>> result;
+        stack<TreeNode *> current_level;
+
+        if (root != nullptr) {
+            current_level.emplace(root);
+        }
+
+        bool normal_order = true;
+        while (!current_level.empty()) {
+            vector<int> level;
+            stack<TreeNode *> next_level;
+            int size = current_level.size();
+            while (!current_level.empty()) {
+                auto *top = current_level.top();
+                current_level.pop();
+                level.emplace_back(top->val);
+                if (normal_order) {
+                    if (top->left != nullptr) {
+                        next_level.emplace(top->left);
+                    }
+                    if (top->right != nullptr) {
+                        next_level.emplace(top->right);
+                    }
+                } else {
+                    if (top->right != nullptr) {
+                        next_level.emplace(top->right);
+                    }
+                    if (top->left != nullptr) {
+                        next_level.emplace(top->left);
+                    }
+                }
+            }
+            result.emplace_back(move(level));
+            normal_order = !normal_order;
+            swap(current_level, next_level);
+        }
+
+        return result;
+    }
+};
+
+// Useing queue.
+class Solution2 {
     /**
      * @param root: The root of binary tree.
      * @return: A list of lists of integer include
