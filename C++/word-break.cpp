06421039d01c273a1eb7231c7f9447906ab48870
kamyu <kamyu104@users.noreply.github.com>
2016-05-22 11:41:20 +0800

    Update word-break.cpp

--- C++/word-break.cpp
+++ C++/word-break.cpp
@@ -1,4 +1,4 @@
-// Time:  O(n^2)
+// Time:  O(n * l^2), l is the max length of the words.
 // Space: O(n)
 
 class Solution {
@@ -9,35 +9,23 @@ public:
      */
     bool wordBreak(string s, unordered_set<string> &dict) {
         const int n = s.length();
-        if (n < 1) {
-            return true;
+
+        size_t max_len = 0;
+        for (const auto& str: dict) {
+            max_len = max(max_len, str.length());
         }
 
-        // Filter out impossible string which alphabet set is not covered
-        // by dict.
-        unordered_set<char> chrs;
-        for (const auto& word : dict) {
-            for (const auto& c : word) {
-                chrs.emplace(c);
-            }
-        } 
-        for (const auto& c : s) {
-            if (chrs.find(c) == chrs.end())
-                return false;
-        }
-
-        // DP
-        vector<bool> canBreak(n, false);
-        for (int i = 0; i < n; ++i) {
-            for (int j = i; j >= 0; --j) {
-                if ((j == 0 || canBreak[j - 1]) && 
-                    dict.count(s.substr(j, i - j + 1))) {
+        vector<bool> canBreak(n + 1, false);
+        canBreak[0] = true;
+        for (int i = 1; i <= n; ++i) {
+            for (int l = 1; l <= max_len && i - l >= 0; ++l) {
+                if (canBreak[i - l] && dict.count(s.substr(i - l, l))) {
                     canBreak[i] = true;
                     break;
                 }
             }
         }
 
-        return canBreak[n - 1];
+        return canBreak[n];
     }
 };
