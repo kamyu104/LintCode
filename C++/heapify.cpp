d55588f5ff90372721820c99c69e6557df2d075b
kamyu <kamyu104@users.noreply.github.com>
2016-05-22 09:32:56 +0800

    Update heapify.cpp

--- C++/heapify.cpp
+++ C++/heapify.cpp
@@ -1,6 +1,7 @@
 // Time:  O(n)
 // Space: O(1)
 
+// Bottom-Up.
 class Solution {
 public:
     /**
@@ -8,19 +9,48 @@ public:
      * @return: void
      */
     void heapify(vector<int> &A) {
-        for (int i = 0; i < A.size(); ++i) {
-            minHeapify(A, i);
+        for (int i = A.size() / 2; i >= 0; --i) {
+            sift_down(A, i);
         }
     }
 
-    void minHeapify(vector<int>& A, int index) {
+    void sift_down(vector<int>& A, int index) {
+            int smallest = k;
+            if (k * 2 + 1 < A.size() && A[k * 2 + 1] < A[smallest]) {
+                smallest = k * 2 + 1;
+            } else if (k * 2 + 2 < A.size() && A[k * 2 + 2] < A[smallest]) {
+                smallest = k * 2 + 2;
+            }
+            if (smallest != k) {
+                swap(A[smallest], A[k]);
+                k = smallest;
+            }
+    }
+};
+
+
+// Time:  O(nlogn)
+// Space: O(1)
+// Top-Down.
+class Solution2 {
+public:
+    /**
+     * @param A: Given an integer array
+     * @return: void
+     */
+    void heapify(vector<int> &A) {
+        for (int i = 0; i < A.size(); ++i) {
+            sift_up(A, i);
+        }
+    }
+
+    void sift_up(vector<int>& A, int index) {
         while ((index - 1) / 2 >= 0) {
             int parent = (index - 1) / 2;
             if (A[index] < A[parent]) {
                 swap(A[parent], A[index]);
                 index = parent;
-            }
-            else {
+            } else {
                 break;
             }
         }
