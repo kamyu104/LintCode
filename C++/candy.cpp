4f1f2fece12577ce2e258d12e7a38d608d1d70c8
kamyu <kamyu104@users.noreply.github.com>
2016-05-27 21:42:20 +0800

    Update candy.cpp

--- C++/candy.cpp
+++ C++/candy.cpp
@@ -15,9 +15,9 @@ public:
             }
         }
 
-        for (int i = ratings.size() - 1; i >= 1; --i) {
-            if (ratings[i - 1] > ratings[i] && candies[i - 1] <= candies[i]) {
-                candies[i - 1] = candies[i] + 1;
+        for (int i = ratings.size() - 2; i >= 0; --i) {
+            if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
+                candies[i] = candies[i + 1] + 1;
             }
         }
         return accumulate(candies.cbegin(), candies.cend(), 0);
