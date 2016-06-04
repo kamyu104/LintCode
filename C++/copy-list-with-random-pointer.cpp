bff3f64ba990b99d38271b52b51f9c775b61b9aa
kamyu <kamyu104@users.noreply.github.com>
2016-06-04 17:51:17 +0800

    Update copy-list-with-random-pointer.cpp

--- C++/copy-list-with-random-pointer.cpp
+++ C++/copy-list-with-random-pointer.cpp
@@ -17,22 +17,22 @@ public:
      */
     RandomListNode *copyRandomList(RandomListNode *head) {
         // Insert the copied node after the original one.
-        for (RandomListNode *curr = head; curr; curr = curr->next->next) {
-            RandomListNode *node = new RandomListNode(curr->label);
+        for (auto curr = head; curr; curr = curr->next->next) {
+            auto node = new RandomListNode(curr->label);
             node->next = curr->next;
             curr->next = node;
         }
 
         // Update random node.
-        for (RandomListNode *curr = head; curr; curr = curr->next->next) {
+        for (auto curr = head; curr; curr = curr->next->next) {
             if (curr->random) {
                 curr->next->random = curr->random->next;
             }
         }
 
         // Seperate the copied nodes from original ones.
-        RandomListNode dummy(0);
-        for (RandomListNode *curr = head, *copy_curr = &dummy;
+        RandomListNode dummy{0};
+        for (auto curr = head, copy_curr = &dummy;
              curr;
              copy_curr = copy_curr->next, curr = curr->next) {
             copy_curr->next = curr->next;
