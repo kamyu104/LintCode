a09319c038c954e00c354671bb7993f15850ae84
kamyu <kamyu104@users.noreply.github.com>
2016-06-04 13:17:08 +0800

    Update merge-two-sorted-lists.cpp

--- C++/merge-two-sorted-lists.cpp
+++ C++/merge-two-sorted-lists.cpp
@@ -23,6 +23,7 @@ public:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
         ListNode dummy = ListNode(0);
         auto *curr = &dummy;
+
         while (l1 && l2) {
             if (l1->val <= l2->val) {
                 curr->next = l1;
@@ -34,6 +35,7 @@ public:
             curr = curr->next;
         }
         curr->next = l1 ? l1 : l2;
+
         return dummy.next;
     }
 };
