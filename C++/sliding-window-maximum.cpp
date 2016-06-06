88316bfda3d309a1ea907e285f94e44ad0f51b00
kamyu <kamyu104@users.noreply.github.com>
2016-06-06 10:11:56 +0800

    Update sliding-window-maximum.cpp

--- C++/sliding-window-maximum.cpp
+++ C++/sliding-window-maximum.cpp
@@ -12,16 +12,16 @@ public:
         vector<int> max_numbers;
 
         for (int i = 0; i < nums.size(); ++i) {
-    		while ((!dq.empty() && nums[i] > nums[dq.back()])) {
-    			dq.pop_back();
-    		} 
-    		dq.emplace_back(i);
-    		if (i - k >= 0  && dq.front() == i - k) {
-    		    dq.pop_front();
-    		}
-    		if (i >= k - 1) {
-    			max_numbers.emplace_back(nums[dq.front()]);
-    		}
+            while ((!dq.empty() && nums[i] > nums[dq.back()])) {
+                dq.pop_back();
+            } 
+            dq.emplace_back(i);
+            if (i - k >= 0  && dq.front() == i - k) {
+                dq.pop_front();
+            }
+            if (i >= k - 1) {
+                max_numbers.emplace_back(nums[dq.front()]);
+            }
         }
 
         return max_numbers;
