e7218357946ccabfab08b5d132d6b9425f066b49
kamyu <kamyu104@users.noreply.github.com>
2016-06-06 09:44:46 +0800

    Update trapping-rain-water-ii.cpp

--- C++/trapping-rain-water-ii.cpp
+++ C++/trapping-rain-water-ii.cpp
@@ -1,21 +1,9 @@
-// Time:  O(m * n * (logm + logn))
+// Time:  O(m * n * log(m + n) ~ O(m * n * (log(m * n)))
 // Space: O(m * n)
 
 // BFS with priority queue (min heap), refactored version.
 class Solution {
 public:
-    struct Cell {
-        int i;
-        int j;
-        int height;
-    };
-
-    struct Compare {
-        bool operator()(const Cell& a, const Cell& b) {
-            return a.height > b.height;
-        }
-    };
-
     /**
      * @param heights: a matrix of integers
      * @return: an integer
@@ -37,17 +25,17 @@ public:
             heap_.emplace(Cell{0, j, heights[0][j]});
             heap_.emplace(Cell{m_ - 1, j, heights[m_ - 1][j]});
         }
-
+        const vector<pair<int, int>> directions{{0, -1}, {0, 1},
+                                                {-1, 0}, {1, 0}};
         // BFS with priority queue (min heap)
         while (!heap_.empty()) {
             Cell c = heap_.top();
             heap_.pop();
             is_visited_[c.i][c.j] = true;
 
-            trap += fill(heights, c.i + 1, c.j, c.height); // Up
-            trap += fill(heights, c.i - 1, c.j, c.height); // Down
-            trap += fill(heights, c.i, c.j + 1, c.height); // Right
-            trap += fill(heights, c.i, c.j - 1, c.height); // Left
+	        for (const auto& d : directions) {
+                trap += fill(heights, c.i + d.first, c.j + d.second, c.height);
+	        }
         }
 
         return trap;
@@ -70,84 +58,20 @@ private:
         return 0;
     }
 
-    int m_;
-    int n_;
-    vector<vector<bool>> is_visited_;
-    priority_queue<Cell ,vector<Cell>, Compare> heap_; // Use min heap to get the lowerest cell.
-};
-
-// Time:  O(m * n * (logm + logn))
-// Space: O(m * n)
-// BFS with priority queue (min heap)
-class Solution2 {
-public:
     struct Cell {
         int i;
         int j;
         int height;
     };
 
-    /**
-     * @param heights: a matrix of integers
-     * @return: an integer
-     */
-    int trapRainWater(vector<vector<int>> &heights) {
-        const int m = heights.size();
-        const int n = heights[0].size();
-        int trap = 0;
-        vector<vector<bool>> is_visited(m, vector<bool>(n, false));
-
-        // Use min heap to get the lowerest Cell.
-        auto comp = [](const Cell& a, const Cell& b ) { return a.height > b.height; };
-        priority_queue<Cell , vector<Cell>, decltype(comp)> heap(comp);
-
-        // Put the Cells on the border into min heap.
-        for (int i = 0; i < m; ++i) {
-            heap.emplace(Cell{i, 0, heights[i][0]});
-            heap.emplace(Cell{i, n - 1, heights[i][n - 1]});
+    struct Compare {
+        bool operator()(const Cell& a, const Cell& b) {
+            return a.height > b.height;
         }
-        for (int j = 0; j < n; ++j) {
-            heap.emplace(Cell{0, j, heights[0][j]});
-            heap.emplace(Cell{m - 1, j, heights[m - 1][j]});
-        }
+    };
 
-        // BFS with priority queue (min heap)
-        while (!heap.empty()) {
-            // Get the lowest Cell.
-            Cell c = heap.top();
-            heap.pop();
-            is_visited[c.i][c.j] = true;
-
-            // Up
-            if (c.i + 1 < m && !is_visited[c.i + 1][c.j]) {
-                is_visited[c.i + 1][c.j] = true;
-                trap += max(0, c.height - heights[c.i + 1][c.j]); // Fill in the gap.
-                heap.emplace(Cell{c.i + 1, c.j, max(c.height,
-                                                    heights[c.i + 1][c.j])});
-            }
-            // Down
-            if (c.i - 1 >= 0 && !is_visited[c.i - 1][c.j]) {
-                is_visited[c.i - 1][c.j] = true;
-                trap += max(0, c.height - heights[c.i - 1][c.j]); // Fill in the gap.
-                heap.emplace(Cell{c.i - 1, c.j, max(c.height,
-                                                    heights[c.i - 1][c.j])});
-            }
-            // Right
-            if (c.j + 1 < n && !is_visited[c.i][c.j + 1]) {
-                is_visited[c.i][c.j + 1] = true;
-                trap += max(0, c.height - heights[c.i][c.j + 1]); // Fill in the gap.
-                heap.emplace(Cell{c.i, c.j + 1, max(c.height,
-                                                    heights[c.i][c.j + 1])});
-            }
-            // Left
-            if (c.j - 1 >= 0 && !is_visited[c.i][c.j - 1]) {
-                is_visited[c.i][c.j - 1] = true;
-                trap += max(0, c.height - heights[c.i][c.j - 1]); // Fill in the gap.
-                heap.emplace(Cell{c.i, c.j - 1, max(c.height,
-                                                    heights[c.i][c.j - 1])});
-            }
-        }
-
-        return trap;
-    }
+    int m_;
+    int n_;
+    vector<vector<bool>> is_visited_;
+    priority_queue<Cell ,vector<Cell>, Compare> heap_; // Use min heap to get the lowerest cell.
 };
