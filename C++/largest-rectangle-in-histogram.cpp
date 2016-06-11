34941963387b2a4299e53d252177ef8c71dd155d
kamyu <kamyu104@users.noreply.github.com>
2016-06-11 13:30:42 +0800

    Update largest-rectangle-in-histogram.cpp

--- C++/largest-rectangle-in-histogram.cpp
+++ C++/largest-rectangle-in-histogram.cpp
@@ -10,29 +10,17 @@ public:
     int largestRectangleArea(vector<int> &height) {
         stack<int> increasing_height;
         int max_area = 0;
-        int i = 0;
 
-        while (i <= height.size()) {  // "i == height.size()" is to handle remaning heights in stack.
+        for (int i = 0; i <= height.size();) {
             if (increasing_height.empty() ||
                 (i < height.size() && height[i] > height[increasing_height.top()])) {
-                // Stores height in a strictly increasing order.
                 increasing_height.emplace(i);
                 ++i;
             } else {
-                // Compute all largest rectangles of heights in stack which height >= current height.
-                int last = increasing_height.top();
+                auto h = height[increasing_height.top()];
                 increasing_height.pop();
-                if (increasing_height.empty()) {
-                    // Left bound: 0
-                    // Right bound: i - 1
-                    // Height: height[last]
-                    max_area = max(max_area, height[last] * (i - 1 - (-1)));
-                } else {
-                    // Left bound: increasing_height.top() + 1
-                    // Right bound: i - 1
-                    // Height: height[last]
-                    max_area = max(max_area, height[last] * (i - 1 - increasing_height.top()));
-                }
+                auto left = increasing_height.empty() ? -1 : increasing_height.top();
+                max_area = max(max_area, h * (i - left - 1));
             }
         }
 
