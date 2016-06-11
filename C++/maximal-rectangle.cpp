66f423c7a6bef9794ee0f57f1123084997b27c7c
kamyu <kamyu104@users.noreply.github.com>
2016-06-11 13:26:59 +0800

    Update maximal-rectangle.cpp

--- C++/maximal-rectangle.cpp
+++ C++/maximal-rectangle.cpp
@@ -1,7 +1,55 @@
 // Time:  O(m * n)
 // Space: O(n)
 
+// Increasing stack solution.
 class Solution {
+public:
+    /**
+     * @param matrix a boolean 2D matrix
+     * @return an integer
+     */
+    int maximalRectangle(vector<vector<bool> > &matrix) {
+        if (matrix.empty() || matrix[0].empty()) {
+            return 0;
+        }
+
+        int res = 0;
+        vector<int> height(matrix[0].size(), 0);
+        for (int i = 0; i < matrix.size(); ++i) {
+            for (int j = 0; j < matrix[0].size(); ++j) {
+                height[j] = matrix[i][j] == 1 ? height[j] + 1 : 0;
+            }
+            res = max(res, largestRectangleArea(height));
+        }
+
+        return res;
+    }
+
+private:
+    int largestRectangleArea(const vector<int> &height) {
+        stack<int> increasing_height;
+        int max_area = 0;
+
+        for (int i = 0; i <= height.size();) {
+            if (increasing_height.empty() ||
+                (i < height.size() && height[i] > height[increasing_height.top()])) {
+                increasing_height.emplace(i);
+                ++i;
+            } else {
+                auto h = height[increasing_height.top()];
+                increasing_height.pop();
+                auto left = increasing_height.empty() ? -1 : increasing_height.top();
+                max_area = max(max_area, h * (i - left - 1));
+            }
+        }
+        return max_area;
+    }
+};
+
+// Time:  O(m * n)
+// Space: O(n)
+// DP solution.
+class Solution2 {
 public:
     /**
      * @param matrix a boolean 2D matrix
