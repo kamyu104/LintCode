d0484f0fdb24bcea7e49a5e72aef25fdfef729c4
kamyu <kamyu104@users.noreply.github.com>
2016-06-12 12:02:50 +0800

    Update permutations-ii.cpp

--- C++/permutations-ii.cpp
+++ C++/permutations-ii.cpp
@@ -9,15 +9,15 @@ public:
      */
     vector<vector<int>> permuteUnique(vector<int> &nums) {
         vector<vector<int>> result;
-        deque<bool> used(nums.size(), false);
+        vector<bool> used(nums.size(), false);
         vector<int> ans;
 
         sort(nums.begin(), nums.end());
-        permuteUniqueRecu(nums, &used, &ans, &result);
+        permuteUniqueHelper(nums, &used, &ans, &result);
         return result;
     }
 
-    void permuteUniqueRecu(const vector<int> &A, deque<bool> *used,
+    void permuteUniqueHelper(const vector<int> &A, vector<bool> *used,
                            vector<int> *ans, vector<vector<int>> *result) {
         if (ans->size() == A.size()) {
             result->emplace_back(*ans);
@@ -25,13 +25,14 @@ public:
         }
 
         for (size_t i = 0; i < A.size(); ++i) {
-            if (!(*used)[i] && !(i != 0 && A[i - 1] == A[i] && (*used)[i - 1])) {
-                (*used)[i] = true;
-                ans->emplace_back(A[i]);
-                permuteUniqueRecu(A, used, ans, result);
-                ans->pop_back();
-                (*used)[i] = false;
+            if ((*used)[i] || (i != 0 && A[i - 1] == A[i] && !(*used)[i - 1])) {
+                continue;
             }
+            (*used)[i] = true;
+            ans->emplace_back(A[i]);
+            permuteUniqueHelper(A, used, ans, result);
+            ans->pop_back();
+            (*used)[i] = false;
         }
     }
 };
