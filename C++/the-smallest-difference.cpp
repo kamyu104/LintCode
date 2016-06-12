d872aa626f9cceaf21808f591c125bc1c2404ad4
kamyu <kamyu104@users.noreply.github.com>
2016-06-12 09:30:28 +0800

    Update the-smallest-difference.cpp

--- C++/the-smallest-difference.cpp
+++ C++/the-smallest-difference.cpp
@@ -1,7 +1,38 @@
-// Time:  O(nlogn)
+// Time:  O(max(m, n) * log(min(m, n)))
 // Space: O(1)
 
 class Solution {
+public:
+    /**
+     * @param A, B: Two integer arrays.
+     * @return: Their smallest difference.
+     */
+    int smallestDifference(vector<int> &A, vector<int> &B) {
+        if (A.size() > B.size()) {
+            return smallestDifference(B, A);
+        }
+        sort(A.begin(), A.end());
+
+
+        int min_diff = numeric_limits<int>::max();
+        for (const auto& b : B) {
+            auto it = lower_bound(A.cbegin(), A.cend(), b);
+            if (it != A.cend()) {
+                min_diff = min(min_diff, *it - b);
+            }
+            if (it != A.cbegin()) {
+                --it;
+                min_diff = min(min_diff, b - *it);
+            }
+        }
+
+        return min_diff;
+    }
+};
+
+// Time:  O(nlogn)
+// Space: O(1)
+class Solution2 {
 public:
     /**
      * @param A, B: Two integer arrays.
