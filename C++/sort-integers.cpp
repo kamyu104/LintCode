a48902ea2e6767003eda87ce51a378ace09b9941
kamyu <kamyu104@users.noreply.github.com>
2016-06-19 20:31:16 +0800

    Update sort-integers.cpp

--- C++/sort-integers.cpp
+++ C++/sort-integers.cpp
@@ -1,6 +1,46 @@
-// Time:  O(n^2)
+// Time:  O(n) ~ O(n^2)
 // Space: O(1)
 
+// In-place inertion sort.
+class Solution {
+public:
+    /**
+     * @param A an integer array
+     * @return void
+     */
+    void sortIntegers(vector<int>& A) {
+        for (int n = 1; n < A.size(); ++n) {
+            for (int i = n - 1; i >= 0 && A[i] > A[i + 1]; --i) {
+                swap(A[i], A[i + 1]);
+            }
+        }
+    }
+};
+
+// Time:  O(n^2)
+// Space: O(1)
+// Selection sort.
+class Solution {
+public:
+    /**
+     * @param A an integer array
+     * @return void
+     */
+    void sortIntegers(vector<int>& A) {
+        for (int n = 0; n < A.size(); ++n) {
+            int min_idx = n;
+            for (int i = n + 1; i < A.size(); ++i) {
+                if (A[i] < A[min_idx]) {
+                    min_idx = i;
+                }
+            }
+            swap(A[n], A[min_idx]);
+        }
+    }
+};
+
+// Time:  O(n^2)
+// Space: O(1)
 // Bubble sort.
 class Solution {
 public:
