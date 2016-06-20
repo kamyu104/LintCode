9db7c52f403410bc2c141fe99f7f9b3c20e09cd6
kamyu <kamyu104@users.noreply.github.com>
2016-06-20 10:33:12 +0800

    Update maximum-subarray-iii.cpp

--- C++/maximum-subarray-iii.cpp
+++ C++/maximum-subarray-iii.cpp
@@ -1,7 +1,42 @@
-// Time:  O(k * n^2)
+// Time:  O(k * n)
 // Space: O(k * n)
 
 class Solution {
+public:
+    /**
+     * @param nums: A list of integers
+     * @param k: An integer denote to find k non-overlapping subarrays
+     * @return: An integer denote the sum of max k non-overlapping subarrays
+     */
+    int maxSubArray(vector<int> nums, int k) {
+        const int n = nums.size();
+        // local_max[x][y] means the max sum in range [0, x - 1] include nums[x-1]
+        // with y non-overlapping subarrays.
+        vector<vector<int>> local_max(n + 1,
+                                vector<int>(k + 1, numeric_limits<int>::min()));
+        // global_max[x][y] means the max sum in range [0, x - 1]
+        // with y non-overlapping subarrays.
+        vector<vector<int>> global_max(n + 1, vector<int>(k + 1, 0));
+        
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= min(i, k); ++j) {
+                local_max[i][j] = max(local_max[i - 1][j],
+                                      global_max[i - 1][j - 1]) + nums[i - 1];
+                if (j == i) {
+                    global_max[i][j] = local_max[i][j];
+                } else {
+                    global_max[i][j] = max(global_max[i - 1][j],
+                                           local_max[i][j]);
+                }
+            }
+         }
+        return global_max[n][k];
+    }
+};
+
+// Time:  O(k * n^2)
+// Space: O(k * n)
+class Solution2 {
 public:
     /**
      * @param nums: A list of integers
@@ -39,7 +74,7 @@ public:
 
 // Time:  O(k * n^2)
 // Space: O(k * n^2)
-class Solution2 {
+class Solution3 {
 public:
     /**
      * @param nums: A list of integers
