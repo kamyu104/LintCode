6c9b3afb049c6f37dc2089a0ed038d184d96305b
kamyu <kamyu104@users.noreply.github.com>
2016-06-20 09:51:23 +0800

    Update continuous-subarray-sum-ii.cpp

--- C++/continuous-subarray-sum-ii.cpp
+++ C++/continuous-subarray-sum-ii.cpp
@@ -1,5 +1,5 @@
 // Time:  O(n)
-// Space: O(n)
+// Space: O(1)
 
 class Solution {
 public:
@@ -9,85 +9,47 @@ public:
      *          the first number and the index of the last number
      */
     vector<int> continuousSubarraySumII(vector<int>& A) {
-        if (A.empty()) {
-            return {-1, -1};
-        }
+        vector<int> result{-1, -1};
+        int max_sum = numeric_limits<int>::min();
+        int total = 0;
 
-        // Calculates the circular / non-circular solution.
-        vector<int> circular(2), non_circular(2);
-        if (findMaxSubarray(A, &non_circular) >= 
-            findCircularMaxSubarray(A, &circular)) {
-            return non_circular;
-        } else {
-            return circular;
-        }
-    }
-
-    // Calculates the non-circular solution.
-    int findMaxSubarray(const vector<int>& A, vector<int> *max_i_j) {
-        int curr_sum = A[0];
-        int max_sum = curr_sum;
-        for (int i = 0, j = 1; j < A.size(); ++j) {
-            if (curr_sum < 0) {
-                i = j;
-                curr_sum = 0;
-            }
-
-            curr_sum += A[j];
-            if (curr_sum > max_sum) {
-                max_sum = curr_sum;
-                (*max_i_j)[0] = i, (*max_i_j)[1] = j;
-            }
-        }
-
-        return max_sum;
-    }
-
-    // Calculates the solution which is circular.
-    int findCircularMaxSubarray(const vector<int>& A, vector<int> *max_i_j) {
-        // Max subarray sum starts at index 0 and ends at or before index i.
-        vector<int> max_sum_from_start(A.size());
-        vector<int> max_j(A.size());
-        int sum = A.front();
-        max_sum_from_start[0] = sum;
-        max_j[0] = 0;
-        for (int j = 1; j < A.size(); ++j) {
-            sum += A[j];
-            if (sum > max_sum_from_start.back()) {
-                max_sum_from_start[j] = sum;
-                max_j[j] = j;
-            } else {
-                max_sum_from_start[j] = max_sum_from_start[j - 1];
-                max_j[j] = max_j[j - 1];
-            }
-        }
-
-        // Max subarray sum starts at index i + 1 and ends at the last element.
-        vector<int> max_sum_to_end(A.size());
-        vector<int> max_i(A.size());
-        sum = 0;
-        max_sum_to_end.back() = sum;
-        max_i.back() = 0;
-        for (int i = A.size() - 2; i >= 0; --i) {
-            sum += A[i + 1];
-            if (sum > max_sum_to_end[i + 1]) {
-                max_sum_to_end[i] = sum;
-                max_i[i] = i + 1;
-            } else {
-                max_sum_to_end[i] = max_sum_to_end[i + 1];
-                max_i[i] = max_i[i + 1];
-            }
-        }
-
-        // Calculates the max subarray which is circular.
-        int circular_max = INT_MIN;
+        // Non-circular subarray.
+        int sum = 0;
+        int start = 0, end = 0;
         for (int i = 0; i < A.size(); ++i) {
-            if (max_sum_from_start[i] + max_sum_to_end[i] > circular_max) {
-                circular_max = max_sum_from_start[i] + max_sum_to_end[i];
-                (*max_i_j)[0] = max_i[i], (*max_i_j)[1] = max_j[i];
+            total += A[i];
+            if (sum < 0) {
+                sum = A[i];
+                start = end = i;
+            } else {
+                sum += A[i];
+                end = i;
+            }
+            if (sum >= max_sum) {
+                max_sum = sum;
+                result[0] = start;
+                result[1] = end;
             }
         }
 
-        return circular_max;
+        // Circular subarray.
+        sum = 0;
+        start = 0, end = -1;
+        for (int i = 0; i < A.size(); ++i) {
+            if (sum > 0) {
+                sum = A[i];
+                start = end = i;
+            } else {
+                sum += A[i];
+                end = i;
+            }
+            if (total - sum > max_sum && !(start == 0 && end == A.size() - 1)) {
+                max_sum = total - sum;
+                result[0] = (end + 1) % A.size();
+                result[1] = (start - 1) % A.size();
+            }
+        }
+
+        return result;
     }
 };
