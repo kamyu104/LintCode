55cf8d7376603b31682804b26c312841494ae9ea
kamyu <kamyu104@users.noreply.github.com>
2016-07-06 22:15:40 +0800

    Update fast-power.cpp

--- C++/fast-power.cpp
+++ C++/fast-power.cpp
@@ -8,17 +8,17 @@ public:
      * @return: An integer
      */
     int fastPower(int a, int b, int n) {
-        return longFastPower(a % b, b, n);
-    }
-
-    long long longFastPower(long long a, int b, int n) {
         if (n == 0) {
             return 1 % b;
         }
-        if (n % 2 == 1) {
-            return (a * longFastPower((a * a) % b, b, n / 2)) % b;
+        if (n == 1) {
+            return a % b;
+        }
+        long long tmp = fastPower(a, b, n / 2);
+        if (n % 2 == 0) {
+            return (tmp * tmp) % b;
         } else {
-            return longFastPower((a * a) % b, b, n / 2);
+            return ((tmp * tmp) % b * a) % b;
         }
     }
 };
