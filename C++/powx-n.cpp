087272e22e2fcc009180712238bce834572d0226
kamyu <kamyu104@users.noreply.github.com>
2016-07-11 22:17:57 +0800

    Update powx-n.cpp

--- C++/powx-n.cpp
+++ C++/powx-n.cpp
@@ -1,4 +1,4 @@
-// Time:  O(logn)
+// Time:  O(logn) = O(1)
 // Space: O(1)
 
 // Iterative solution.
@@ -23,8 +23,8 @@ public:
     }
 };
 
-// Time:  O(logn)
-// Space: O(logn)
+// Time:  O(logn) = O(1)
+// Space: O(logn) = O(1)
 // Recursive solution.
 class Solution2 {
 public:
