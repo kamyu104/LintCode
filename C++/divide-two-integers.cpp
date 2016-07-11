d6b9d5bb22a486f6f3142cca1f6ebf75ddd31636
kamyu <kamyu104@users.noreply.github.com>
2016-07-11 21:58:52 +0800

    Update divide-two-integers.cpp

--- C++/divide-two-integers.cpp
+++ C++/divide-two-integers.cpp
@@ -11,43 +11,32 @@ public:
      */
     int divide(int dividend, int divisor) {
         // Handle corner case.
-        if (divisor == INT_MIN) {
-            return dividend == divisor ? 1 : 0;
-        } else if (!divisor || divisor == -1 && dividend == INT_MIN) {
+        if (dividend == numeric_limits<int>::min() && divisor == -1) {
             return numeric_limits<int>::max();
         } else if (divisor == 1) {
             return dividend;
         }
 
-        bool negative = (dividend > 0) ^ (divisor > 0);
-        if (dividend > 0) {
-            dividend = -dividend;
-        }
-        if (divisor > 0) {
-            divisor = -divisor;
-        }
+        int a = dividend > 0 ? -dividend : dividend;
+        int b = divisor > 0 ? -divisor : divisor;
 
-        int product = divisor;
-        int idx = 1;
-        while (product < 0 && idx < 32) {
-            ++idx;
-            product <<= 1;
+        int shift = 0;
+        while (b << shift < 0 && shift < 31) {
+            ++shift;
         }
-        idx -= 2; // Skip value of INT_MIN
-        product = divisor << idx;
-        int multiplier = 1 << idx;
+        shift -= 1;
 
-        int res = 0;
-        while (dividend <= divisor) {
-            if (dividend <= product) {
-                res += multiplier;
-                dividend -= product;
+        int result = 0;
+        while (shift >= 0) {
+            if (a <= b << shift) {
+                a -= b << shift;
+                result += 1 << shift;
             }
-            --idx;
-            product = divisor << idx;
-            multiplier >>= 1;
+            --shift;
         }
-        return negative ? -res : res;
+
+        result =  (dividend ^ divisor) >> 31 ? -result : result;
+        return result;
     }
 };
 
