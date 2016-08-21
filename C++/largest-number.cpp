035fc5522c4155de02873baa7d167c4a6c653499
kamyu <kamyu104@users.noreply.github.com>
2016-08-21 20:00:59 +0800

    Update largest-number.cpp

--- C++/largest-number.cpp
+++ C++/largest-number.cpp
@@ -15,12 +15,12 @@ public:
 
         // combine the numbers
         string max_num;
-        for (auto& i : num) {
+        for (const auto& i : num) {
             max_num.append(to_string(i));
         }
 
         // special case: start with zero (e.g. [0, 0])
-        if (max_num[0] == '0')  {
+        if (!max_num.empty() && max_num[0] == '0')  {
             return "0";
         }
 
