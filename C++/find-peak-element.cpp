747dfa44ed598082b154e6fdd0781123984391f5
kamyu <kamyu104@users.noreply.github.com>
2017-08-25 00:09:51 +0800

    Update find-peak-element.cpp

--- C++/find-peak-element.cpp
+++ C++/find-peak-element.cpp
@@ -12,10 +12,7 @@ public:
         
         while (left < right) {
             const auto mid = left + (right - left) / 2;
-            if ((mid == 0 || A[mid - 1] < A[mid]) &&
-               (mid + 1 == A.size() || A[mid] > A[mid + 1])) {
-                return mid;
-            } else if (!(mid == 0 || A[mid - 1] < A[mid])) {
+            if (A[mid] > A[mid + 1]) {
                 right = mid;
             } else {
                 left = mid + 1;
